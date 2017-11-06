5dfa7529960977e6893fd66fdd0d47a5a23c3b2e
Niek Candaele <niekcandaele@gmail.com>
2017-11-06 19:46:42 +0100

    Player model and controller added

---
+++ api/models/Player.js
@@ -0,0 +1,81 @@
+/**
+ * Player.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+
+        //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+        //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+        //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+        steamID: {
+            type: 'number',
+            required: true
+        },
+
+        entityID: {
+            type: 'number'
+        },
+
+        ip: {
+            type: 'string',
+            required: true
+        },
+
+        name: {
+            type: 'string',
+            required: true
+        },
+
+        positionX: {
+            type: "number"
+        },
+
+        positionY: {
+            type: "number"
+        },
+
+        positionZ: {
+            type: "number"
+        },
+
+        playtime: {
+            type: "number"
+        },
+
+        lastOnline: {
+            type: "string"
+        },
+
+        banned: {
+            type: 'boolean',
+            required: true
+        },
+
+
+        //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+        //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+        //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+        //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+        //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+        //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+        server: {
+            model: 'sdtdServer'
+        },
+
+        user: {
+            model: 'user'
+        }
+
+    },
+
+};
