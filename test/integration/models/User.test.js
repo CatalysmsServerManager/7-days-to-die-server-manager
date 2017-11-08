85b0b1ab15795b53488a57f4b3ea963852d1930b
Niek Candaele <niekcandaele@gmail.com>
2017-11-08 22:22:19 +0100

    Config changes

--- test/integration/models/User.test.js
+++ test/integration/models/User.test.js
@@ -1,27 +1,3 @@
-// describe('User (model)', function() {
-
-//       describe('#findBestStudents()', function() {
-//         it('should return 5 users', function (done) {
-//           User.findBestStudents()
-//           .then(function(bestStudents) {
-
-//             if (bestStudents.length !== 5) {
-//               return done(new Error(
-//                 'Should return exactly 5 students -- the students '+
-//                 'from our test fixtures who are considered the "best".  '+
-//                 'But instead, got: '+util.inspect(bestStudents, {depth:null})+''
-//               ));
-//             }//-•
-
-//             return done();
-
-//           })
-//           .catch(done);
-//         });
-//       });
-
-//     });
-
 var supertest = require('supertest');
 
 describe('User (model)', function() {
