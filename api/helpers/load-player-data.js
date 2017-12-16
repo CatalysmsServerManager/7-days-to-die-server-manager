7e7c6f629b35ed5be686788458dfa27987389543
Niek Candaele <niekcandaele@gmail.com>
2017-12-16 14:17:36 +0100

    CSMM-27 load-player-data helper added

--- api/helpers/load-player-data.js
+++ api/helpers/load-player-data.js
@@ -1,108 +1,120 @@
 var sevenDays = require('machinepack-7daystodiewebapi');
 
 module.exports = {
-
-
   friendlyName: 'Load player data',
-
-
-  description: '',
-
-
+  description: 'Load player information from a 7 Days to die server',
   inputs: {
-    serverID: {
-      type: 'number'
+    serverId: {
+      type: 'number',
+      required: true
     },
-
+    steamId: {
+      type: 'string'
+    },
+    playerId: {
+      type: 'number'
+    }
   },
-
-
   exits: {
+    error: {
+      friendlyName: 'error'
+    }
+  },
+  fn: async function (inputs, exits) {
+    sails.log.debug(`HELPER LOAD PLAYER DATA Loading player data for server ${inputs.serverId}`);
 
-    noPlayers: {
-      description: 'No player data on the server.'
+    try {
+      let server = await SdtdServer.findOne(inputs.serverId)
+      let playerList = await getPlayerList(server)
+      let newPlayerList = await playerList.players.map(await updatePlayerInfo)
+      let jsonToSend = await createJSON(newPlayerList)
+      exits.success(jsonToSend)
+    } catch (error) {
+      exits.error(error)
     }
 
-  },
 
-
-  fn: async function(inputs, exits) {
-
-    await sails.models.sdtdserver.findOne(inputs.serverID).exec(async function(err, foundServer) {
-      if (err) {return exits.error('load-player-data database error' + err);}
-      await sevenDays.getPlayerList({
-        ip: foundServer.ip,
-        port: foundServer.webPort,
-        authName: foundServer.authName,
-        authToken: foundServer.authToken
-      }).exec({
-        error: function(err) {
-          console.log(err);
-        },
-        success: function(data) {
-          const playerData = data.players;
-          if (_.isUndefined(playerData)) { return exits.noPlayers(); }
-          playerData.forEach(async function(playerJSON) {
-            await sails.models.player.find({
-              where: {
-                steamID: playerJSON.steamid,
-                entityID: playerJSON.entityid
-              }
-            }).exec(async function(err, foundPlayer) {
-              if (err) { return console.log(`ERROR finding player ${err}`); }
-
-              // Player was already in our database, updating data
-              if (foundPlayer.id) {
-                try {
-                  await Player.update({ id: foundPlayer.id })
-                    .set({
-                      ip: playerJSON.ip,
-                      positionX: playerJSON.position.x,
-                      positionY: playerJSON.position.y,
-                      positionZ: playerJSON.position.z,
-                      playtime: playerJSON.totalplaytime,
-                      lastOnline: playerJSON.lastonline,
-                      banned: playerJSON.banned
-                    });
-                } catch (error) {
-                  sails.log(`Error updating player: ${error}`);
-                }
-
-              }
-
-              // Player not in database, creating a new record
-              if (_.isUndefined(foundPlayer.id)) {
-                try {
-                  await Player.create({
-                    steamID: playerJSON.steamid,
-                    entityID: playerJSON.entityid,
-                    ip: playerJSON.ip,
-                    name: playerJSON.name,
-                    positionX: playerJSON.position.x,
-                    positionY: playerJSON.position.y,
-                    positionZ: playerJSON.position.z,
-                    playtime: playerJSON.totalplaytime,
-                    lastOnline: playerJSON.lastonline,
-                    banned: playerJSON.banned,
-                    server: inputs.serverID
-                  });
-                } catch (error) {
-                  sails.log(`Error creating player: ${error}`);
-                }
-
-              }
-
-              // All done.
-              return exits.success();
-            });
-          });
-
+    async function updatePlayerInfo(newPlayer) {
+      return new Promise(async function(resolve) {
+        try {
+          foundOrCreatedPlayer = await Player.findOrCreate({
+            steamId: newPlayer.steamid,
+            server: inputs.serverId,
+            entityId: newPlayer.entityid
+          }, {
+            steamId: newPlayer.steamid,
+            server: inputs.serverId,
+            entityId: newPlayer.entityid,
+            name: newPlayer.name,
+            ip: newPlayer.ip
+          })
+          newPlayer = await Player.update({
+            steamId: foundOrCreatedPlayer.steamId,
+            server: inputs.serverId,
+            entityId: foundOrCreatedPlayer.entityId
+          }).set({
+            ip: newPlayer.ip,
+            positionX: newPlayer.position.x,
+            positionY: newPlayer.position.y,
+            positionZ: newPlayer.position.z,
+            playtime: newPlayer.totalplaytime,
+            banned: newPlayer.banned
+          }).fetch()
+          resolve(newPlayer[0])
+        } catch (error) {
+          throw error
         }
-      });
 
-    });
+      })
+    }
 
-  }
+    async function getPlayerList(server) {
+      return new Promise(resolve => {
+        sevenDays.getPlayerList({
+          ip: server.ip,
+          port: server.webPort,
+          authName: server.authName,
+          authToken: server.authToken
+        }).exec({
+          error: function (err) {
+            throw err
+          },
+          success: function (playerList) {
+            resolve(playerList)
+          }
+        });
+      })
+    }
 
 
-};
+    async function createJSON(playerList) {
+      return new Promise(async function(resolve) {
+        try {
+          let toSend = {};
+          Promise.all(playerList).then(resolvedPlayers => {
+            toSend.totalPlayers = playerList.length
+            toSend.players = new Array();
+            resolvedPlayers.forEach(function(player) {
+              let playerData = new Object()
+              playerData.id = player.id
+              playerData.steamId = player.steamId
+              playerData.entityId = player.entityId
+              playerData.location = new Object()
+              playerData.location.x = player.positionX
+              playerData.location.y = player.positionY
+              playerData.location.z = player.positionZ
+              playerData.totalPlaytime = player.playtime
+              playerData.banned = player.banned
+              playerData.server = player.server
+              toSend.players.push(playerData)
+            })
+            resolve(toSend)
+          })
+          
+        } catch (error) {
+          throw error
+        }
+      })
+    }
+  },
+}
