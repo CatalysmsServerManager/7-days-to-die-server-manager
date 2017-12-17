8b240b60e637ea9781e5e8f287408dce95c88d95
Niek Candaele <niekcandaele@gmail.com>
2017-12-17 12:36:21 +0100

    CSMM-29 Implemented tests with supertest

--- test/API/player.api.test.js
+++ test/API/player.api.test.js
@@ -1,20 +1,38 @@
 var supertest = require('supertest');
-var assert = require('assert');
 var agent = supertest(sails.hooks.http.app);
 
 describe('GET /api/player/inventory @api', function () {
-    it('should return JSON', function (done) {
-      agent
-        .get('/api/player/inventory')
-        .query({
-          playerId: sails.testPlayer.id
-        })
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
-    it('should error when no playerId given', function (done) {
-      agent
-        .get('/api/player/inventory')
-        .expect(400, done);
-    });
+  it('should return JSON', function (done) {
+    agent
+      .get('/api/player/inventory')
+      .query({
+        steamId: sails.testUser.steamId,
+        serverId: sails.testServer.id
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(response => {
+        if (_.isUndefined(response.body.inventory) || _.isUndefined(response.body.inventory.bag) || _.isUndefined(response.body.inventory.belt) || _.isUndefined(response.body.inventory.equipment)) {
+          return done('Did not find inventory information')
+
+        }
+        done()
+      })
   });
+  it('should error when no steamId given', function (done) {
+    agent
+      .get('/api/player/inventory')
+      .query({
+        serverId: sails.testServer.id
+      })
+      .expect(400, done);
+  });
+  it('should error when no serverId given', function (done) {
+    agent
+      .get('/api/player/inventory')
+      .query({
+        steamId: sails.testUser.steamId,
+      })
+      .expect(400, done);
+  });
+});
