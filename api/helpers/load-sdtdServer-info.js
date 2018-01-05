a90144754f32c67148a56bd19d9aad614ec59480
Niek Candaele <niekcandaele@gmail.com>
2018-01-05 15:16:51 +0100

    Added basic day7 command

---
+++ api/helpers/load-sdtdServer-info.js
@@ -0,0 +1,104 @@
+var sevenDays = require('machinepack-7daystodiewebapi');
+
+module.exports = {
+
+
+  friendlyName: 'Load sdtdServer info',
+
+
+  description: 'Performs several API requests to a sdtd server',
+
+
+  inputs: {
+    serverId: {
+      friendlyName: 'Server ID',
+      required: true,
+      example: 1
+    }
+  },
+
+
+  exits: {
+    success: {
+      outputFriendlyName: 'Success'
+    },
+    connectionError: {
+      description: 'Could not connect to the 7 days to die server'
+    },
+    databaseError: {
+      description: 'Error reading or writing data to DB'
+    }
+  },
+
+    /**
+     * @description Loads server information
+     * @name loadSdtdServerInfo
+     * @param {number} serverId
+     * @memberof module:Helpers
+     * @method
+     */
+
+
+  fn: async function (inputs, exits) {
+
+    try {
+
+      // Load serverinfo from DB first
+      let server = await SdtdServer.findOne(inputs.serverId);
+      let data = new Object();
+      data.stats = await loadStats(server)
+      data.serverInfo = await loadServerInfo(server)
+      exits.success(data)
+    } catch (error) {
+      return exits.error(error)
+    }
+
+
+    function loadStats(server) {
+      return new Promise((resolve, reject) => {
+        sevenDays.getStats({
+          ip: server.ip,
+          port: server.webPort,
+          authName: server.authName,
+          authToken: server.authToken
+        }).exec({
+          error: error => {
+            sails.log.error(error);
+            reject(error);
+          },
+          success: data => {
+            resolve(data)
+          }
+        })
+      })
+    }
+
+    function loadServerInfo(server) {
+      return new Promise((resolve, reject) => {
+        sevenDays.getServerInfo({
+          ip: server.ip,
+          port: server.webPort,
+          authName: server.authName,
+          authToken: server.authToken
+        }).exec({
+          error: error => {
+            sails.log.error(error);
+            reject(error);
+          },
+          success: data => {
+              for (const dataPoint in data) {
+                  if (data.hasOwnProperty(dataPoint)) {
+                      data[dataPoint] = data[dataPoint].value
+                  }
+              }
+            resolve(data)
+          }
+        })
+      })
+    }
+
+
+  }
+
+
+};
---
+++ api/hooks/discordBot/commands/7dtd/day7.js
@@ -0,0 +1,49 @@
+const Commando = require('discord.js-commando');
+
+/**
+ * @memberof module:DiscordCommands
+ * @name Day7
+ * @description Displays basic information about a 7 Days to Die server
+ */
+
+class Day7 extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'day7',
+      group: '7dtd',
+      memberName: 'day7',
+      description: 'Shows basic information about a server',
+      details: "For more information see the website"
+    });
+  }
+
+  async run(msg, args) {
+
+    try {
+      let sdtdServer = await SdtdServer.find({
+        discordGuildId: msg.guild.id
+      }).limit(1)
+      sails.helpers.loadSdtdserverInfo(sdtdServer[0].id).exec((err, serverInfo) => {
+        if (err) {
+          throw err
+        }
+        let resultEmbed = new msg.client.customEmbed();
+        resultEmbed
+        .setColor('GREEN')
+        .addField('Online Players', serverInfo.stats.players, true)
+        .addField('Ping', serverInfo.serverInfo.Ping, true)
+        .addField('Hostiles / Animals', `${serverInfo.stats.hostiles} / ${serverInfo.stats.animals}`)
+        .setFooter(serverInfo.serverInfo.GameHost)
+
+        msg.channel.send(resultEmbed)
+      })
+    } catch (error) {
+      sails.log.error(error)
+    }
+
+
+  }
+}
+
+
+module.exports = Day7;
