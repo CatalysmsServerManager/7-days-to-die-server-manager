18b0feccb57bc706aaa836b3bb0d2849fa706b81
Niek Candaele <niekcandaele@gmail.com>
2018-01-10 13:07:55 +0100

    Added winston custom logger

--- config/log.js
+++ config/log.js
@@ -10,20 +10,20 @@
  * https://sailsjs.com/docs/concepts/logging
  */
 
+var winston = require('winston');
+var customLogger = new winston.Logger();
+
+// A console transport logging debug and above.
+customLogger.add(winston.transports.Console, {
+  level: 'debug',
+  colorize: true
+});
+
 module.exports.log = {
+  // Pass in our custom logger, and pass all log levels through.
+  custom: customLogger,
+  level: 'silly',
 
-  /***************************************************************************
-  *                                                                          *
-  * Valid `level` configs: i.e. the minimum log level to capture with        *
-  * sails.log.*()                                                            *
-  *                                                                          *
-  * The order of precedence for log levels from lowest to highest is:        *
-  * silly, verbose, info, debug, warn, error                                 *
-  *                                                                          *
-  * You may also set the level to "silent" to suppress all logs.             *
-  *                                                                          *
-  ***************************************************************************/
-
-  // level: 'info'
-
+  // Disable captain's log so it doesn't prefix or stringify our meta data.
+  inspect: false
 };
