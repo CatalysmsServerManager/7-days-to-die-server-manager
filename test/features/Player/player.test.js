0c96e163a071234dd0c03622e7667fba0032875b
Niek Candaele <niekcandaele@gmail.com>
2018-01-17 10:57:37 +0100

    CSMM-52 CSMM-2 Add feature tests for player info(s)

---
+++ test/features/Player/player.test.js
@@ -0,0 +1,23 @@
+Feature('Player features');
+
+// All these tests start on server dashboard
+
+Before((I) => {
+  I.amOnPage('/');
+  I.addTestServer();
+});
+
+After((I) => {
+  I.deleteTestServer();
+})
+
+Scenario('See online players on dashboard', (I) => {
+  I.see('Online players');
+});
+
+Scenario('View full player list', (I) => {
+  I.click('View all players');
+  I.see('Name');
+  I.see('Location');
+  I.click('Back to dashboard')
+});
--- test/features/steps_file.js
+++ test/features/steps_file.js
@@ -1,36 +1,43 @@
-
 'use strict';
 // in this file you can append custom step methods to 'I' object
 
-module.exports = function() {
+module.exports = function () {
   return actor({
 
-    loginSteam: async function() {
+    loginSteam: async function () {
       this.amOnPage('/auth/steam');
-      this.fillField('username', process.env.CSMM_TEST_STEAM_USERNAME);
-      this.fillField('password', process.env.CSMM_TEST_STEAM_PASSWORD);
+      if (this.see('username')) {
+        this.fillField('username', process.env.CSMM_TEST_STEAM_USERNAME);
+        this.fillField('password', process.env.CSMM_TEST_STEAM_PASSWORD);
+      }
       this.click('#imageLogin');
       this.waitForElement('.navbar', 10);
       I.see('#steam-avatar');
-  },
+    },
 
-  logout: async function() {
-    this.amOnPage('/');
-    this.loginSteam();
-    this.amOnPage('/auth/logout');
-    this.dontSee('#steam-avatar')
-  },
+    logout: async function () {
+      this.amOnPage('/');
+      this.loginSteam();
+      this.amOnPage('/auth/logout');
+      this.dontSee('#steam-avatar')
+    },
 
-  addTestServer: async function() {
-    this.loginSteam();
-    this.amOnPage('/sdtdserver/addserver');
-    this.fillField('Server IP', process.env.CSMM_TEST_IP);
-    this.fillField('Web port', process.env.CSMM_TEST_WEBPORT);
-    this.fillField('Telnet port', process.env.CSMM_TEST_TELNETPORT);
-    this.fillField('Telnet password', process.env.CSMM_TEST_TELNETPW);
-    this.click('Submit');
-    I.see('Dashboard');
-  }
+    addTestServer: async function () {
+      this.loginSteam();
+      this.amOnPage('/sdtdserver/addserver');
+      this.fillField('Server IP', process.env.CSMM_TEST_IP);
+      this.fillField('Web port', process.env.CSMM_TEST_WEBPORT);
+      this.fillField('Telnet port', process.env.CSMM_TEST_TELNETPORT);
+      this.fillField('Telnet password', process.env.CSMM_TEST_TELNETPW);
+      this.click('Submit');
+      this.waitForElement('#dashboard-title', 10);
+    },
+
+    // Assumes you are on dashboard already !
+    deleteTestServer: async function () {
+      this.waitForElement("#delete-server-button", 10);
+      this.click('Delete server');
+    }
 
   });
 }
