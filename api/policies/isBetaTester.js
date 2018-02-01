5e755c73efb9b181ebddf532f93c70a6c8d5c902
Niek Candaele <niekcandaele@gmail.com>
2018-02-01 12:50:37 +0100

    Changed log level for isBetaTester policy

--- api/policies/isBetaTester.js
+++ api/policies/isBetaTester.js
@@ -5,9 +5,9 @@
  *
  */
 module.exports = function isBetaTester(req, res, next) {
-    sails.log.debug(`POLICY - isBetaTester - Check if a user is logged in`);
+    sails.log.silly(`POLICY - isBetaTester - Check if a user is logged in`);
     if( sails.config.custom.betaTesters.includes(req.signedCookies.userProfile.steamId) ) {
-      sails.log.debug(`POLICY - isBetaTester - ${req.signedCookies.userProfile.userName} User ${req.signedCookies.userProfile.id} is a beta tester!`);
+      sails.log.silly(`POLICY - isBetaTester - ${req.signedCookies.userProfile.userName} User ${req.signedCookies.userProfile.id} is a beta tester!`);
       return next();
     } else {
       sails.log.debug(`POLICY - isBetaTester - User is not in beta testers list`);
