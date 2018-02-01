b061a0e989314bf97ff191de10e2e91d892fdb17
Niek Candaele <niekcandaele@gmail.com>
2018-02-01 18:39:19 +0100

    CSMM-91 Added pause button Reworked the console class abit aswell

--- assets/js/sdtdServer/console.js
+++ assets/js/sdtdServer/console.js
@@ -1,25 +1,18 @@
 class sdtdConsole {
   constructor(serverId) {
     this.serverId = serverId;
+    this.start()
   }
 
   start() {
-
+    this.status = true;
     io.socket.get('/sdtdserver/' + this.serverId + '/socket', function (response) {});
-
-    io.socket.on('logLine', function addNewLogLine(logLine) {
-      if (logLine.msg.includes("error")) {
-        $('.console-window').append('<li class=\"log-line text-danger\">' + logLine.msg + '</li>');
-      } else {
-        $('.console-window').append('<li class=\"log-line\">' + logLine.msg + '</li>');
-      }
-
-      $('.console-window').scrollTop($('.console-window')[0].scrollHeight);
-    });
+    io.socket.on('logLine', addNewLogLine);
   }
 
   stop() {
-    io.socket.removeListener('logLine', addNewLogLine);
+    this.status = false;
+    io.socket.off('logLine', addNewLogLine);
   }
 
   executeCommand(command) {
@@ -39,4 +32,14 @@ class sdtdConsole {
       });
     });
   }
+}
+
+function addNewLogLine(logLine) {
+  if (logLine.msg.includes("error")) {
+    $('.console-window').append('<li class=\"log-line text-danger\">' + logLine.msg + '</li>');
+  } else {
+    $('.console-window').append('<li class=\"log-line\">' + logLine.msg + '</li>');
+  }
+
+  $('.console-window').scrollTop($('.console-window')[0].scrollHeight);
 }
