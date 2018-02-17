bc1f92b45f23bfbb5ea2d280eecd0940e4543f00
Niek Candaele <niekcandaele@gmail.com>
2018-02-17 17:55:35 +0100

    fixes niekcandaele/CSMM-issues#23

--- assets/js/sdtdServer/chat.js
+++ assets/js/sdtdServer/chat.js
@@ -2,6 +2,8 @@ class sdtdChat {
   constructor(serverId) {
     this.serverId = serverId;
     this.addNewChatMessage = addNewChatMessage.bind(this);
+    this.addPlayerConnectedMessage = addPlayerConnectedMessage.bind(this)
+    this.addPlayerDisconnectedMessage = addPlayerDisconnectedMessage.bind(this)
     this.start();
   }
 
@@ -13,6 +15,8 @@ class sdtdChat {
       console.log('Subscribed to socket ' + response);
     });
     io.socket.on('chatMessage', this.addNewChatMessage);
+    io.socket.on('playerConnected', this.addPlayerConnectedMessage)
+    io.socket.on('playerDisconnected', this.addPlayerDisconnectedMessage)
     addSavedMessagesToChatWindow(this.serverId);
   }
 
@@ -38,6 +42,18 @@ class sdtdChat {
       });
     });
   }
+}
+
+function addPlayerConnectedMessage(connectedMessage) {
+  $('.chat-window').append(`<li class=\"chat-message\">${connectedMessage.playerName} connected </li>`);
+  $('.chat-window').scrollTop($('.chat-window')[0].scrollHeight);
+  addMessageToStorage(`${connectedMessage.playerName} connected`, this.serverId)
+}
+
+function addPlayerDisconnectedMessage(disconnectedMessage) {
+  $('.chat-window').append(`<li class=\"chat-message\">${disconnectedMessage.playerName} left </li>`);
+  $('.chat-window').scrollTop($('.chat-window')[0].scrollHeight);
+  addMessageToStorage(`${disconnectedMessage.playerName} left`, this.serverId)
 }
 
 function addNewChatMessage(chatMessage) {
