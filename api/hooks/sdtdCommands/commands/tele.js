2e2c16cbe7fec887455df9a9579347e771566efd
niekcandaele <niekcandaele@gmail.com>
2018-03-27 13:42:34 +0200

    initial tele command

--- api/hooks/sdtdCommands/commands/tele.js
+++ api/hooks/sdtdCommands/commands/tele.js
@@ -1,4 +1,5 @@
 let SdtdCommand = require('../command.js');
+const sevenDays = require('machinepack-7daystodiewebapi');
 
 class tele extends SdtdCommand {
   constructor(serverId) {
@@ -9,7 +10,93 @@ class tele extends SdtdCommand {
   }
 
   async run(chatMessage, playerId) {
-    console.log(chatMessage)
+
+    let server = await SdtdServer.findOne({
+      id: this.serverId
+    }).populate('config');
+    let player = await Player.findOne({
+      id: playerId
+    });
+    let playerTeleports = await PlayerTeleport.find({ player: playerId });
+
+    let args = chatMessage.messageText.split(' ');
+    args.splice(0, 1)
+
+
+    if (!server.config[0].enabledPlayerTeleports) {
+      return sevenDays.sendMessage({
+        ip: server.ip,
+        port: server.webPort,
+        authName: server.authName,
+        authToken: server.authToken,
+        message: `This command is disabled! Ask your server admin to enable this.`,
+        playerId: player.steamId
+      }).exec({
+        error: (error) => {
+          sails.log.error(`HOOK - SdtdCommands - Failed to respond to player`);
+        },
+        success: (result) => {
+          return;
+        }
+      });
+    }
+
+    let teleportFound = false
+    playerTeleports.forEach(teleport => {
+      if (teleport.name == args[0]) {
+        teleportFound = teleport
+      }
+    })
+
+    if (!teleportFound) {
+      return sevenDays.sendMessage({
+        ip: server.ip,
+        port: server.webPort,
+        authName: server.authName,
+        authToken: server.authToken,
+        message: `No teleport with that name found`,
+        playerId: player.steamId
+      }).exec({
+        error: (error) => {
+          sails.log.error(`HOOK - SdtdCommands - Failed to respond to player`);
+        },
+        success: (result) => {
+          return;
+        }
+      });
+    }
+
+    sevenDays.teleportPlayer({
+      ip: server.ip,
+      port: server.webPort,
+      authName: server.authName,
+      authToken: server.authToken,
+      playerId: player.steamId,
+      coordinates: `${teleportFound.x} ${teleportFound.y} ${teleportFound.z}`
+    }).exec({
+      success: (response) => {
+        return sevenDays.sendMessage({
+          ip: server.ip,
+          port: server.webPort,
+          authName: server.authName,
+          authToken: server.authToken,
+          message: `Woosh! Welcome to ${teleportFound.name}`,
+          playerId: player.steamId
+        }).exec({
+          error: (error) => {
+            sails.log.error(`HOOK - SdtdCommands - Failed to respond to player`);
+          },
+          success: (result) => {
+            return;
+          }
+        });
+      },
+      error: (error) => {
+        sails.log.error(`Hook - sdtdCommands:teleport - ${error}`);
+        return exits.error(error);
+      }
+    });
+
   }
 }
 
