3b0446479abcfab7170671538fc3abf586e9a6fc
niekcandaele <niekcandaele@gmail.com>
2018-04-11 16:46:44 +0200

    Added function to check if a discord user is server admin

---
+++ api/hooks/discordBot/util/checkIfAdmin.js
@@ -0,0 +1,30 @@
+
+/**
+ * @description Checks if a discord user is registered as admin for a server
+ * @param {string} discordUserId Id of the discord user
+ * @param {number} sdtdServerId Id of the sdtdServer
+ */
+
+async function checkIfAdmin(discordUserId, sdtdServerId) {
+    
+    try {
+        const sdtdServer = await SdtdServer.findOne({id: sdtdServerId}).populate('admins').populate('owner');
+        let isAdmin = false;
+        sdtdServer.admins.forEach(admin => {
+            if (admin.discordId == discordUserId) {
+                isAdmin = true
+            }
+        })
+        if (sdtdServer.owner.discordId == discordUserId) {
+            isAdmin = true
+        }
+        return isAdmin
+    } catch (error) {
+        sails.log.error(`HOOK - discordBot:checkIfAdmin - ${error}`)
+    }
+
+
+}
+
+module.exports = checkIfAdmin
+
