9ccde5cc05e4b13af1cb0d9ba3ec886543b3e2f2
niekcandaele <niekcandaele@gmail.com>
2018-04-15 17:07:42 +0200

    get-player-balance helper

---
+++ api/helpers/economy/get-player-balance.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+
+  friendlyName: 'Get player balance',
+
+
+  description: 'Gets a players current balance (rounded down to nearest int)',
+
+
+  inputs: {
+
+    playerId: {
+      type: 'number',
+      description: 'Id of the player',
+      required: true
+    }
+
+  },
+
+
+  exits: {
+    success: {
+      outputFriendlyName: 'Success',
+      outputType: 'number'
+    },
+  },
+
+  fn: async function (inputs, exits) {
+
+    try {
+        let player = await Player.findOne(inputs.playerId);
+
+        let currentCurrency = Math.round(player.currency);
+        return exits.success(currentCurrency);
+    } catch (error) {
+        sails.log.error(`HELPER economy:get-player-balance - ${error}`);
+        return exits.error(error);
+    }
+
+  }
+};
+
