3143bdcd2e1ce51682c96acefc2c430ffc0a36a5
niekcandaele <niekcandaele@gmail.com>
2018-04-15 22:26:33 +0200

    playtimeEarner first version

---
+++ api/hooks/economy/index.js
@@ -0,0 +1,131 @@
+var sevenDays = require('machinepack-7daystodiewebapi');
+const PlaytimeEarner = require('./objects/playtimeEarner');
+
+let playtimeEarnerMap = new Map();
+
+
+module.exports = function economy(sails) {
+
+
+    return {
+        initialize: function (cb) {
+
+            sails.on('hook:discordbot:loaded', async function () {
+                
+                let economyEnabledServers = await SdtdConfig.find({
+                    economyEnabled: true
+                });
+                sails.log.info(`HOOK: economy - Initializing ${economyEnabledServers.length} servers`);
+                for (let config of economyEnabledServers) {
+                    await startPlaytimeEarner(config.server)
+                }
+
+                return cb();
+            });
+
+
+        },
+
+        start: async function (serverId, type) {
+
+            switch (type) {
+                case 'playtimeEarner':
+                    return startPlaytimeEarner(serverId);
+                    break;
+
+                default:
+                    throw new Error('Unknown updateObject type')
+                    break;
+            }
+        },
+
+        stop: async function (serverId, type) {
+            switch (type) {
+                case 'playtimeEarner':
+                    return stopPlaytimeEarner(serverId);
+                    break;
+
+                default:
+                    throw new Error('Unknown updateObject type')
+                    break;
+            }
+        },
+
+        getStatus: function (server, type) {
+            switch (type) {
+                case 'playtimeEarner':
+                    return playtimeEarnerMap.has(String(server.id ? server.id : server))
+                    break;
+
+                default:
+                    throw new Error('Unknown updateObject type')
+                    break;
+            }
+        }
+
+    };
+}
+
+
+async function startPlaytimeEarner(serverId) {
+    try {
+        const server = await SdtdServer.findOne(serverId).populate('config');
+        const config = server.config[0];
+        let loggingObject = await sails.hooks.sdtdlogs.getLoggingObject(server.id);
+        let playtimeEarnerObject = new PlaytimeEarner(server, config, loggingObject);
+        await playtimeEarnerObject.start();
+        setMap(server, playtimeEarnerObject);
+        return true
+    } catch (error) {
+        sails.log.error(`HOOK - economy - Error starting playtimeEarner ${error}`)
+        return false
+    }
+}
+
+
+async function stopPlaytimeEarner(serverId) {
+    try {
+        let playtimeEarnerObject = await getMap(serverId);
+        playtimeEarnerObject.stop();
+        deleteMap(serverId, playtimeEarnerObject);
+    } catch (error) {
+        sails.log.error(`HOOK - economy - Error stopping playtimeEarner ${error}`)
+    }
+}
+
+
+function getMap(server, type) {
+    switch (type) {
+        case 'playtimeEarner':
+            return playtimeEarnerMap.get(String(server.id ? server.id : server))
+            break;
+
+        default:
+            throw new Error('Unknown updateObject type')
+            break;
+    }
+}
+
+function setMap(server, updateObject) {
+    switch (updateObject.type) {
+        case 'playtimeEarner':
+            return playtimeEarnerMap.set(String(server.id ? server.id : server), updateObject);
+            break;
+
+        default:
+            throw new Error('Must set a known type in updateObject')
+            break;
+    }
+}
+
+function deleteMap(server, updateObject) {
+    switch (updateObject.type) {
+        case 'playtimeEarner':
+            return playtimeEarnerMap.delete(String(server.id ? server.id : server), updateObject);
+            break;
+
+        default:
+            throw new Error('Must set a known type in updateObject')
+            break;
+    }
+}
---
+++ api/hooks/economy/objects/playtimeEarner.js
@@ -0,0 +1,47 @@
+class PlaytimeEarner {
+    constructor(server, config, loggingObject) {
+        this.loggingObject = loggingObject
+        this.server = server
+        this.config = config
+        this.type = 'playtimeEarner'
+        this.interval = undefined
+        this.intervalFunc = loadOnlinePlayersAndAwardMoney.bind(this);
+        // Bind a updateListener function with this class so we can access server and config objects inside the event listener
+        // this.updateListener = this._updateListener.bind(this);
+    }
+
+    async start() {
+        try {
+            sails.log.debug(`Started playtime earner for server ${this.server.name}`);
+
+            this.interval = setInterval(this.intervalFunc, 300000)
+        } catch (error) {
+            sails.log.error(error)
+        }
+
+
+    }
+
+    async stop() {
+        sails.log.debug(`Stopped playtime earner for server ${this.server.name}`);
+        clearInterval(this.interval);
+
+    }
+
+
+}
+
+
+module.exports = PlaytimeEarner
+
+
+async function loadOnlinePlayersAndAwardMoney() {
+    try {
+        let onlinePlayers = await sails.helpers.loadPlayerData.with({ serverId: this.server.id, onlyOnline: true});
+        onlinePlayers.players.forEach(async player => {
+            await sails.helpers.economy.giveToPlayer.with({playerId: player.id, amountToGive: 1})
+        })
+    } catch (error) {
+        sails.log.error(error)
+    }
+}
