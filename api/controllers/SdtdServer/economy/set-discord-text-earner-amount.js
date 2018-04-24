fe22a80dab2c039c7cfb75d09c8e7d246a8041af
niekcandaele <niekcandaele@gmail.com>
2018-04-24 18:45:50 +0200

    API routes to control text earner settings

---
+++ api/controllers/SdtdServer/economy/set-discord-text-earner-amount.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+    friendlyName: 'Set discordTextEarner amount',
+
+    inputs: {
+        serverId: {
+            type: 'number',
+            required: true
+        },
+
+        amount: {
+            type: 'number',
+            required: true,
+            min: 0
+        }
+    },
+
+    exits: {
+    },
+
+
+    fn: async function (inputs, exits) {
+
+        try {
+            await SdtdConfig.update({ server: inputs.serverId }, { discordTextEarnerAmountPerMessage: inputs.amount });
+            await sails.hooks.economy.reload(inputs.serverId, 'discordTextEarner');
+            await HistoricalInfo.create({
+                type: 'economy',
+                economyAction: 'config',
+                server: inputs.serverId,
+                message: `set discord text earner amount to ${inputs.amount}`
+            });
+            return exits.success();
+        } catch (error) {
+            sails.log.error(`API set-discordTextEarner-amount - ${error}`);
+            return exits.error();
+        }
+
+
+
+    }
+};
---
+++ api/controllers/SdtdServer/economy/set-discord-text-earner-timeout.js
@@ -0,0 +1,42 @@
+module.exports = {
+
+    friendlyName: 'Set discordTextEarner timeout',
+
+    inputs: {
+        serverId: {
+            type: 'number',
+            required: true
+        },
+
+        timeout: {
+            type: 'number',
+            required: true,
+            min: 0
+        }
+    },
+
+    exits: {
+    },
+
+
+    fn: async function (inputs, exits) {
+
+        try {
+            await SdtdConfig.update({ server: inputs.serverId }, { discordTextEarnerTimeout: inputs.timeout });
+            await sails.hooks.economy.reload(inputs.serverId, 'discordTextEarner');
+            await HistoricalInfo.create({
+                type: 'economy',
+                economyAction: 'config',
+                server: inputs.serverId,
+                message: `set discord text earner timeout to ${inputs.timeout}`
+            });
+            return exits.success();
+        } catch (error) {
+            sails.log.error(`API set-discordTextEarner-timeout - ${error}`);
+            return exits.error();
+        }
+
+
+
+    }
+};
