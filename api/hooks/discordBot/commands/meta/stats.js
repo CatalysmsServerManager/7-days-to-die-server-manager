684aa6ed9c6bda1bc5df16f9e17bd56ca4a7eeb1
niekcandaele <niekcandaele@gmail.com>
2018-04-30 12:10:50 +0200

    Add mem usage to stats cmd

--- api/hooks/discordBot/commands/meta/stats.js
+++ api/hooks/discordBot/commands/meta/stats.js
@@ -16,14 +16,17 @@ class Stats extends Commando.Command {
 
     let statsInfo = await sails.helpers.meta.loadSystemStatsAndInfo();
 
+    let memUsage = process.memoryUsage()
+
     let embed = new this.client.customEmbed()
 
     embed.setTitle(`CSMM stats`)
     .addField('Website', `${process.env.CSMM_HOSTNAME}`)
       .addField(`7DTD Servers`, statsInfo.servers, true)
       .addField(`7DTD Players`, statsInfo.players, true)
-      .addField(`Discord guilds: ${statsInfo.guilds}`, `Users: ${statsInfo.users}`, true)
+      .addField(`Discord guilds: ${statsInfo.guilds}`, `Users: ${statsInfo.users}`)
       .addField(`Uptime`, statsInfo.uptime, true)
+      .addField(`Memory usage`, `${Math.round(memUsage.heapUsed / 1024 / 1024)} MB`, true)
       .addField('Modules', `Discord chat bridges: ${statsInfo.chatBridges}
 Country ban modules: ${statsInfo.countryBans}
 MOTD handlers: ${statsInfo.sdtdMotds}
@@ -31,7 +34,7 @@ Ingame command handlers: ${statsInfo.sdtdMotds}`)
 .addField(`Players have teleported ${statsInfo.amountOfTimesTeleported} times`, `There are ${statsInfo.amountOfTeleports} teleport locations`)
 .addField(`Players have executed ${statsInfo.amountOfCustomCommandsExecuted} custom commands`, `There are ${statsInfo.amountOfCustomCommands} custom commands registered`)
 .addField(`Players' average wealth is ${Math.round(statsInfo.currencyAvg)}`,`${Math.round(statsInfo.currencyTotal)} units of currency in circulation`)
-
+.setColor('RANDOM')
 
     msg.channel.send(embed)
 
