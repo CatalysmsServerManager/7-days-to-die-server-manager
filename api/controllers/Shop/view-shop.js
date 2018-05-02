e3b7c440c232ba6b2d97faaae6b958b679082f82
niekcandaele <niekcandaele@gmail.com>
2018-05-02 19:44:35 +0200

    Hide/show buttons on shop page depending if user is server admin

--- api/controllers/Shop/view-shop.js
+++ api/controllers/Shop/view-shop.js
@@ -36,9 +36,9 @@ module.exports = {
     fn: async function (inputs, exits) {
 
         try {
-            let server = await SdtdServer.findOne(inputs.serverId);
+            let server = await SdtdServer.findOne(inputs.serverId).populate('admins');
             let listings = await ShopListing.find({ server: inputs.serverId });
-
+            
             if (_.isUndefined(this.req.session.userId)) {
                 return exits.notLoggedIn("You must be logged in to view a shop.")
             }
@@ -47,12 +47,30 @@ module.exports = {
 
             let player = await Player.find({ server: server.id, user: user.id }).limit(1)
 
+            if (player.length === 0) {
+                return exits.notLoggedIn('Not a valid player profile. Make sure you have logged in to this server. If you think this message is a mistake, please report this issue.')
+            }
+
+            let isAdmin = false;
+
+            if (server.owner === user.id) {
+                isAdmin = true
+            }
+
+            server.admins.forEach(adminProfile => {
+                if (adminProfile.id === user.id) {
+                    isAdmin = true
+                }
+            })
+
             server = _.omit(server, 'authName', 'authToken', 'webPort')
 
             return exits.success({
                 server: server,
                 listings: listings,
-                player: player[0]
+                player: player[0],
+                user: user,
+                isAdmin: isAdmin
             })
 
         } catch (error) {
