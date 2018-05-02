5a7175430949e607e6f4b824d93febf83af24a44
niekcandaele <niekcandaele@gmail.com>
2018-05-02 17:47:44 +0200

    Add player info to shop view

--- api/controllers/Shop/view-shop.js
+++ api/controllers/Shop/view-shop.js
@@ -21,6 +21,12 @@ module.exports = {
             description: '',
             responseType: 'view',
             viewTemplatePath: 'sdtdServer/economy/shop'
+        },
+
+        notLoggedIn: {
+            description: '',
+            responseType: 'badRequest',
+            statusCode: 400
         }
 
     },
@@ -33,11 +39,20 @@ module.exports = {
             let server = await SdtdServer.findOne(inputs.serverId);
             let listings = await ShopListing.find({ server: inputs.serverId });
 
+            if (_.isUndefined(this.req.session.userId)) {
+                return exits.notLoggedIn("You must be logged in to view a shop.")
+            }
+
+            let user = await User.findOne(this.req.session.userId);
+
+            let player = await Player.find({ server: server.id, user: user.id }).limit(1)
+
             server = _.omit(server, 'authName', 'authToken', 'webPort')
 
             return exits.success({
                 server: server,
-                listings: listings
+                listings: listings,
+                player: player[0]
             })
 
         } catch (error) {
