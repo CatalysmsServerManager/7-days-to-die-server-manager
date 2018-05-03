4cc68834698ef72c4e1a4504dab399b18e153b68
niekcandaele <niekcandaele@gmail.com>
2018-05-03 19:39:49 +0200

    first version of new cfg

--- config/env/production.js
+++ config/env/production.js
@@ -18,43 +18,43 @@
  * For more best practices and tips, see:
  * https://sailsjs.com/docs/concepts/deployment
  */
-const winston = require('winston');
+// const winston = require('winston');
 
-customLogger = new winston.Logger({
-  transports: [
-    new winston.transports.File({
-      level: 'info',
-      name: 'infolog',
-      timestamp: true,
-      humanReadableUnhandledException: true,
-      filename: './logs/prod.log',
-      tailable: true,
-      maxsize: 1000,
-      maxFiles: 3,
-      json: false,
-      colorize: true
-    }),
-    new winston.transports.File({
-      level: 'debug',
-      name: 'debuglog',
-      timestamp: true,
-      humanReadableUnhandledException: true,
-      filename: './logs/debug.log',
-      tailable: true,
-      maxsize: 1000,
-      maxFiles: 5,
-      json: false,
-      colorize: true
-    })
-  ]
-})
-
-
+// customLogger = new winston.Logger({
+//   transports: [
+//     new winston.transports.File({
+//       level: 'info',
+//       name: 'infolog',
+//       timestamp: true,
+//       humanReadableUnhandledException: true,
+//       filename: './logs/prod.log',
+//       tailable: true,
+//       maxsize: 1000,
+//       maxFiles: 3,
+//       json: false,
+//       colorize: true
+//     }),
+//     new winston.transports.File({
+//       level: 'debug',
+//       name: 'debuglog',
+//       timestamp: true,
+//       humanReadableUnhandledException: true,
+//       filename: './logs/debug.log',
+//       tailable: true,
+//       maxsize: 1000,
+//       maxFiles: 5,
+//       json: false,
+//       colorize: true
+//     })
+//   ]
+// })
 
 
-const { exec } = require('child_process');
-exec(`"/home/i107/7-Days-To-Die-Server-Manager/node_modules/frontail/bin/frontail" -h ${process.env.CSMM_IP} -p 2010 -n 20 -t dark --ui-highlight -U ${process.env.LOG_USER} -P ${process.env.LOG_PW} /home/i107/7-Days-To-Die-Server-Manager/logs/prod.log`);
-exec(`"/home/i107/7-Days-To-Die-Server-Manager/node_modules/frontail/bin/frontail" -h ${process.env.CSMM_IP} -p 2011 -n 20 --ui-highlight -U ${process.env.LOG_USER} -P ${process.env.LOG_PW} /home/i107/7-Days-To-Die-Server-Manager/logs/debug.log`);
+
+
+// const { exec } = require('child_process');
+// exec(`"/home/i107/7-Days-To-Die-Server-Manager/node_modules/frontail/bin/frontail" -h ${process.env.CSMM_IP} -p 2010 -n 20 -t dark --ui-highlight -U ${process.env.LOG_USER} -P ${process.env.LOG_PW} /home/i107/7-Days-To-Die-Server-Manager/logs/prod.log`);
+// exec(`"/home/i107/7-Days-To-Die-Server-Manager/node_modules/frontail/bin/frontail" -h ${process.env.CSMM_IP} -p 2011 -n 20 --ui-highlight -U ${process.env.LOG_USER} -P ${process.env.LOG_PW} /home/i107/7-Days-To-Die-Server-Manager/logs/debug.log`);
 
 
 
@@ -85,8 +85,8 @@ module.exports = {
          *                                                                          *
          ***************************************************************************/
     default: {
-      adapter: 'sails-mysql',
-      url: process.env.DBSTRING
+      adapter: 'sails-disk',
+    //  url: process.env.DBSTRING
     },
 
   },
@@ -232,7 +232,7 @@ module.exports = {
          *                                                                          *
          ***************************************************************************/
     cookie: {
-      secure: true,
+      secure: false,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
 
@@ -304,13 +304,13 @@ module.exports = {
 
 
 
-  log: {
-    // Pass in our custom logger, and pass all log levels through.
-    custom: customLogger,
+  // log: {
+  //   // Pass in our custom logger, and pass all log levels through.
+  //   custom: customLogger,
 
-    // Disable captain's log so it doesn't prefix or stringify our meta data.
-    inspect: false
-  },
+  //   // Disable captain's log so it doesn't prefix or stringify our meta data.
+  //   inspect: false
+  // },
 
 
 
@@ -339,7 +339,7 @@ module.exports = {
          * (https://sailsjs.com/config/http)                                        *
          *                                                                          *
          ***************************************************************************/
-    trustProxy: true,
+ //   trustProxy: true,
 
   },
 
@@ -354,8 +354,8 @@ module.exports = {
      * this, just try deploying without setting it and see if it works.)       *
      *                                                                         *
      ***************************************************************************/
-  port: 443,
-  explicitHost: "54.36.0.1",
+  // port: 1338,
+  // explicitHost: "54.36.0.1",
 
 
   /**************************************************************************
@@ -377,11 +377,11 @@ module.exports = {
      **************************************************************************/
 
 
-  ssl: {
-    ca: require('fs').readFileSync(require('path').resolve(__dirname,'./ssl/chain.pem')),
-    key: require('fs').readFileSync(require('path').resolve(__dirname,'./ssl/privkey.pem')),
-    cert: require('fs').readFileSync(require('path').resolve(__dirname,'./ssl/fullchain.pem'))
-  },
+  // ssl: {
+  //   ca: require('fs').readFileSync(require('path').resolve(__dirname,'./ssl/chain.pem')),
+  //   key: require('fs').readFileSync(require('path').resolve(__dirname,'./ssl/privkey.pem')),
+  //   cert: require('fs').readFileSync(require('path').resolve(__dirname,'./ssl/fullchain.pem'))
+  // },
 
 
 
