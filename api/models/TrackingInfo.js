5f03f5b6d44ab0ddf7becc62fc7a78a68aa5dc30
niekcandaele <niekcandaele@gmail.com>
2018-05-29 14:31:21 +0200

    Add tracking info model

---
+++ api/models/TrackingInfo.js
@@ -0,0 +1,55 @@
+/**
+ * TrackingInfo.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    x: {
+      type: 'number',
+    },
+
+    y: {
+      type: 'number',
+    },
+    
+    z: {
+      type: 'number',
+    },
+
+    inventory: {
+      type: 'ref',
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    server: {
+      model: 'sdtdserver',
+      required: true
+    },
+
+    player: {
+      model: 'player',
+      required: true
+    },
+
+  },
+
+};
+
