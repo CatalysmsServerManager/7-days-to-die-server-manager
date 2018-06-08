d80054147ffe1acd20ba096991fd669ca7b3a54b
niekcandaele <niekcandaele@gmail.com>
2018-06-08 14:41:42 +0200

    Add ban entry model

---
+++ api/models/BanEntry.js
@@ -0,0 +1,50 @@
+/**
+ * BanEntry.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    steamId: {
+      type: 'string',
+      required: true
+    },
+
+    // An optional note added by server admins. Containing proof or extra information about the ban
+    note: {
+      type: 'string'
+    },
+
+    bannedUntil: {
+      type: 'number'
+    },
+
+    reason: {
+      type: 'string'
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    server: {
+      model: 'sdtdserver'
+    }
+
+  },
+
+};
+
