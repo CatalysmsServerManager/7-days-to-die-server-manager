551ce4869bce0f369cdd837d8fa7ab8aaddda6ee
niekcandaele <niekcandaele@gmail.com>
2018-06-14 10:55:44 +0200

    Validate command options better

--- api/hooks/sdtdCommands/command.js
+++ api/hooks/sdtdCommands/command.js
@@ -15,7 +15,21 @@ class SdtdCommand {
     this.name = options.name;
     this.description = options.description;
     this.extendedDescription = options.extendedDescription;
-    this.aliases = new Array();
+    this.aliases = options.aliases;
+    this.validateCommand();
+  }
+
+
+  // Make sure the command has valid options
+  async validateCommand() {
+
+    if (_.isUndefined(this.name)) {
+      throw new Error(`Implementation error! Must provide a name for 7dtd commands.`)
+    }
+
+    if (!_.isUndefined(this.aliases) && !_.isArray(this.aliases)) {
+      throw new Error(`Aliases must be an array or undefined.`)
+    }
   }
 
 
