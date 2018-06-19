63c148c744b4d0590fed23e69702fcad5987c8f0
niekcandaele <niekcandaele@gmail.com>
2018-06-19 15:19:28 +0200

    Frontend for setting notif channel

--- api/hooks/gbl/index.js
+++ api/hooks/gbl/index.js
@@ -5,6 +5,8 @@
  * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
  */
 
+const sevenDays = require('machinepack-7daystodiewebapi');
+
 module.exports = function defineGblHook(sails) {
 
   return {
@@ -15,7 +17,7 @@ module.exports = function defineGblHook(sails) {
      * @param {Function} done
      */
     initialize: async function (done) {
-      sails.on('hook:discordbot:loaded', async () => {
+      sails.on('hook:sdtdlogs:loaded', async () => {
 
         sails.log.info('Initializing custom hook (`gbl`)');
 
@@ -50,7 +52,55 @@ async function refreshBans() {
 
     try {
 
-        await sails.helpers.sdtd.loadBans(server.id);
+      await sails.helpers.sdtd.loadBans(server.id);
+
+      let loggingObj = sails.hooks.sdtdlogs.getLoggingObject(server.id);
+
+      loggingObj.on('playerConnected', async (connectedMsg) => {
+
+        let foundBans = await BanEntry.find({ steamId: connectedMsg.steamID });
+        let config = await SdtdConfig.findOne({ server: connectedMsg.server.id });
+
+        if (foundBans.length >= config.gblNotificationBans && config.gblNotificationBans != 0) {
+          let player = await Player.findOne({ server: connectedMsg.server.id, steamId: connectedMsg.steamID });
+          await sails.hooks.discordnotifications.sendNotification({
+            serverId: connectedMsg.server.id,
+            notificationType: 'gblmaxban',
+            player: player,
+            bans: foundBans
+          })
+        }
+
+        if (foundBans.length >= config.gblAutoBanBans && config.gblAutoBanEnabled) {
+          let player = await Player.findOne({ server: connectedMsg.server.id, steamId: connectedMsg.steamID });
+          sevenDays.banPlayer({
+            ip: server.ip,
+            port: server.webPort,
+            authName: server.authName,
+            authToken: server.authToken,
+            reason: `CSMM GBL autoban: you are listed ${foundBans.length} times on the global ban list.`,
+            duration: 100,
+            durationUnit: 'years',
+            playerId: connectedMsg.steamID
+          }).exec({
+            success: async (response) => {
+              await sails.hooks.discordnotifications.sendNotification({
+                serverId: connectedMsg.server.id,
+                notificationType: 'gblmaxban',
+                player: player,
+                bans: foundBans,
+                banned: true
+              })
+              sails.log.info(`Autobanned a player for being on GBL too often`, connectedMsg);
+            },
+            error: err => {
+              sails.log.warn(`Error auto banning a player in GBL hook - ${err}`);
+              sails.log.error(err)
+            }
+          })
+        }
+
+      })
 
     } catch (error) {
       sails.log.warn(`Error refreshing ban info for server ${server.name}`, error)
