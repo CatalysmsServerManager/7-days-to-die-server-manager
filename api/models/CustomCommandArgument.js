80fe5e47924cab0926fd7c888bcc2b191066e15b
Niek Candaele <niekcandaele@gmail.com>
2018-06-28 18:49:41 +0200

    Argument model config

---
+++ api/models/CustomCommandArgument.js
@@ -0,0 +1,48 @@
+module.exports = {
+  
+    attributes: {
+  
+      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+      key: {
+          type: 'string',
+          required: true
+      },
+
+      type: {
+          type: 'string',
+          isIn: ['number', 'text', 'setValues'],
+          required: true
+      },
+
+      required: {
+          type: 'boolean',
+          defaultsTo: true
+      },
+
+      default: {
+          type: 'string'
+      },
+
+       
+  
+      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+  
+  
+      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  
+      command: {
+        model: 'CustomCommand',
+        required: true
+      },
+  
+    },
+  
+  };
+  
