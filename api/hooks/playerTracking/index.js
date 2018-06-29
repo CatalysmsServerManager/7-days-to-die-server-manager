48ecb81d4c945f8bda6e147ca1828c8ba6fe75c6
Niek Candaele <niekcandaele@gmail.com>
2018-06-29 08:28:31 +0200

    Limit the info shown on tracking error

--- api/hooks/playerTracking/index.js
+++ api/hooks/playerTracking/index.js
@@ -117,7 +117,7 @@ module.exports = function definePlayerTrackingHook(sails) {
         player = player[0]
 
         if (_.isUndefined(player)) {
-          return sails.log.warn(`Did not find a player with steam ID - ${playerStats.steamid}`, players);
+          return sails.log.warn(`Did not find a player with steam ID - ${playerStats.steamid}`, players.map(player => player.steamId));
         }
 
         await Player.update(player.id, statsUpdate);
