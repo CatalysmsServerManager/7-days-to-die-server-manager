fc2c04e09d71931f7e7d7183731badd138073f9f
niekcandaele <niekcandaele@gmail.com>
2018-07-08 11:50:19 +0200

    Add model config

---
+++ api/models/customDiscordNotification.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+    attributes: {
+  
+      //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+      //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+      //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+  
+      stringToSearchFor: {
+        type: 'string',
+        required: true
+      },
+
+      discordChannelId: {
+          type: 'string',
+          required: true
+      },
+ 
+      //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+      //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+      //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+  
+  
+      //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+      //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+      //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  
+      server: {
+        model: 'SdtdServer',
+        required: true
+      },
+  
+    },
+  
+  };
+  
