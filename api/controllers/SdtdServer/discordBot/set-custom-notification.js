b9c195d6bf8163507cabbd7f92b963f1d7e8259a
niekcandaele <niekcandaele@gmail.com>
2018-07-09 18:20:57 +0200

    Add check for max notifs in action

--- api/controllers/SdtdServer/discordBot/set-custom-notification.js
+++ api/controllers/SdtdServer/discordBot/set-custom-notification.js
@@ -24,16 +24,24 @@ module.exports = {
   
     exits: {
       success: {},
-      badChannel: {
+      maximumNotifications: {
         responseType: 'badRequest'
-      },
-      notFound: {
-        responseType: 'notFound'
       }
     },
   
   
     fn: async function (inputs, exits) {
+
+      let donatorRole = await sails.helpers.meta.checkDonatorStatus.with({ serverId: inputs.serverId });
+      let maxCustomNotifications = sails.config.custom.donorConfig[donatorRole].maxCustomNotifications;
+
+      let currentAmountOfNotifs = await CustomDiscordNotification.find({
+        server: inputs.serverId
+      });
+
+      if (currentAmountOfNotifs.length >= maxCustomNotifications) {
+        return exits.maximumNotifications(`You have reached the maximum of custom notifications! You can set a maximum of ${maxCustomNotifications} as ${donatorRole} user.`)
+      }
   
         await CustomDiscordNotification.create({
             stringToSearchFor: inputs.stringToSearchFor,
