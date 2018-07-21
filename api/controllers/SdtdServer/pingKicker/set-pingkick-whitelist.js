598ea68062db5edcafb7a7fde25a22019c964e14
niekcandaele <niekcandaele@gmail.com>
2018-07-21 17:41:19 +0200

    Add action & route for setting pingKick whitelist

---
+++ api/controllers/SdtdServer/pingKicker/set-pingkick-whitelist.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+
+  friendlyName: 'set ping kicker whitelist',
+
+
+  description: '',
+
+
+  inputs: {
+    serverId: {
+      required: true,
+      type: 'string'
+    },
+    whitelist: {
+      required: true,
+      type: 'json',
+    }
+  },
+
+
+  exits: {
+
+    notAnArray: {
+      description: 'Given whitelist is not a valid array',
+      responseType: 'badRequest'
+    },
+
+  },
+
+  fn: async function (inputs, exits) {
+
+    if (!_.isArray(inputs.whitelist)) {
+      return exits.notAnArray();
+    }
+
+    await SdtdConfig.update({
+      server: inputs.serverId
+    }, {
+        pingWhitelist: inputs.whitelist
+    });
+
+    sails.log.info(`Setting whitelist of pingKicker to ${inputs.whitelist} for server ${inputs.serverId}`);
+    return exits.success();
+
+  }
+
+
+};
