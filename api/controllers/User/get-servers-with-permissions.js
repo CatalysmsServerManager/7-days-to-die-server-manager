01bc216bc5d2147d99b763f0a7da5a77944375be
Niek Candaele <niekcandaele@gmail.com>
2018-09-06 11:17:06 +0200

    Add new API endpoint to get user servers with permissions

---
+++ api/controllers/User/get-servers-with-permissions.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+    friendlyName: 'Get servers with permissions',
+  
+    description: 'Find servers for which this user has some permissions',
+  
+    inputs: {
+      userId: {
+        description: 'The ID of the user to look up.',
+        type: 'number',
+        required: true
+      }
+    },
+  
+    exits: {
+      success: {},
+      notFound: {
+        description: 'No player with the specified ID was found in the database.',
+        responseType: 'notFound'
+      }
+    },
+  
+
+    fn: async function (inputs, exits) {
+      sails.log.debug(`API - User:getServersWithPermissions - Getting servers with permissions for user ${inputs.userId}`);
+  
+      try {
+        let foundUser = await User.findOne({id: inputs.userId});
+        
+        let players = await Player.find({steamId: foundUser.steamId}).populate('role').populate('server');
+
+        let objectToSend = new Array();
+
+        players.map(player => {
+          player.server.role = player.role;
+          objectToSend.push(player.server);
+        })
+
+        return exits.success(objectToSend);
+  
+      } catch (error) {
+        sails.log.error(`API - User:getServersWithPermissions - ${error}`);
+        return exits.error(error);
+      }
+  
+    }
+  };
+  
