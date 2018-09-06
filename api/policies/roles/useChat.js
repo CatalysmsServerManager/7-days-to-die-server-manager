b134371373fc5f6f0f1fce21ade54cef454a56ec
Niek Candaele <niekcandaele@gmail.com>
2018-09-06 12:56:10 +0200

    Add more policies to accomodate permission fields

---
+++ api/policies/roles/useChat.js
@@ -0,0 +1,32 @@
+module.exports = async function useChat(req, res, next) {
+
+    if (_.isUndefined(req.session.user)) {
+      return res.redirect('/auth/steam');
+    }
+  
+    if (_.isUndefined(req.param('serverId')) && _.isUndefined(req.query.serverId)) {
+      return res.badRequest(`Error while running "useChat" policy, could not determine server ID.`);
+    }
+  
+    let serverId = _.isUndefined(req.param('serverId')) ? req.query.serverId : req.param('serverId');
+    let server = await SdtdServer.findOne(serverId);
+    let user = req.session.user;
+  
+    let permCheck = await sails.helpers.roles.checkPermission.with({
+      userId: user.id,
+      serverId: server.id,
+      permission: 'useChat'
+    });
+  
+    if (!permCheck.hasPermission) {
+      sails.log.warn(`User ${user.username} tried to access dashboard without sufficient permissions (user is ${permCheck.role.name}).`)
+      return res.view('meta/notauthorized', {
+        role: permCheck.role,
+        requiredPerm: 'useChat'
+      })
+    }
+  
+    next();
+  
+  };
+  
