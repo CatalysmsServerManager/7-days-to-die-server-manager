16d2018c07daee093213327f2fe4faa85e755aba
Niek Candaele <niekcandaele@gmail.com>
2018-09-19 22:48:02 +0200

    Implement new set note functionality

--- api/controllers/gbl/set-note.js
+++ api/controllers/gbl/set-note.js
@@ -3,43 +3,62 @@ const he = require('he');
 module.exports = {
 
 
-    friendlyName: 'Set note',
+  friendlyName: 'Set note',
 
 
-    description: 'Add or edit a note to a ban entry',
+  description: 'Add or edit a note to a ban entry',
 
 
-    inputs: {
+  inputs: {
 
-        banId: {
-            required: true,
-            type: 'number',
-            custom: async function (valueToCheck) {
-                let foundBan = await BanEntry.findOne(valueToCheck);
-                return foundBan
-            }
-        },
-
-        note: {
-            required: true,
-            type: 'string'
-        },
-
+    banId: {
+      required: true,
+      type: 'number',
+      custom: async function (valueToCheck) {
+        let foundBan = await BanEntry.findOne(valueToCheck);
+        return foundBan
+      }
     },
 
-
-    exits: {
-
+    note: {
+      required: true,
+      type: 'string'
     },
 
+  },
 
-    fn: async function (inputs, exits) {
 
-        let updatedRecord = await BanEntry.update({ id: inputs.banId }, { note: he.encode(inputs.note) });
-        sails.log.info(`Update note on ban ${inputs.banId} - ${inputs.note}`)
-        return exits.success(updatedRecord);
-
+  exits: {
+    notAuthorized: {
+      description: '',
+      statusCode: 403
     }
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    let foundBan = await BanEntry.findOne(inputs.banId);
+
+    let permCheck = await sails.helpers.roles.checkPermission.with({
+      userId: this.req.session.user.id,
+      serverId: foundBan.server,
+      permission: 'manageGbl'
+    });
+
+    if (!permCheck.hasPermission) {
+      return exits.notAuthorized()
+    }
+
+    let updatedRecord = await BanEntry.update({
+      id: inputs.banId
+    }, {
+      note: he.encode(inputs.note)
+    });
+    sails.log.info(`Update note on ban ${inputs.banId} - ${inputs.note}`)
+    return exits.success(updatedRecord);
+
+  }
 
 
 };
