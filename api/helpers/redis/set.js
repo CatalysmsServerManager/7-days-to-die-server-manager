949fcff554bbb1007a5d0c8b5e72024cc115ca46
Niek Candaele <niekcandaele@gmail.com>
2018-11-20 15:41:12 +0100

    Default to in memory caching when redis is not enabled

--- api/helpers/redis/set.js
+++ api/helpers/redis/set.js
@@ -37,36 +37,41 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
-    if (inputs.ex) {
-      if (_.isUndefined(inputs.ttl)) {
-        return exits.error(`When settings ex true you must provide a TTL.`)
+    if (process.env.REDISSTRING) {
+
+      if (inputs.ex) {
+        if (_.isUndefined(inputs.ttl)) {
+          return exits.error(`When settings ex true you must provide a TTL.`)
+        }
+
+        sails.getDatastore('cache').leaseConnection(function during(redisConnection, proceed) {
+          redisConnection.set(inputs.keyString, inputs.value, 'EX', inputs.ttl, (err, reply) => {
+            if (err) return proceed(err);
+
+            return proceed(undefined, reply)
+          })
+        }).exec((err, result) => {
+          if (err) return exits.error(err);
+
+          return exits.success(result);
+        })
+
+      } else {
+
+        sails.getDatastore('cache').leaseConnection(function during(redisConnection, proceed) {
+          redisConnection.set(inputs.keyString, inputs.value, (err, reply) => {
+            if (err) return proceed(err);
+
+            return proceed(undefined, reply)
+          })
+        }).exec((err, result) => {
+          if (err) return exits.error(err);
+
+          return exits.success(result);
+        })
       }
-
-      sails.getDatastore('cache').leaseConnection(function during(redisConnection, proceed) {
-        redisConnection.set(inputs.keyString, inputs.value, 'EX', inputs.ttl, (err, reply) => {
-          if (err) return proceed(err);
-
-          return proceed(undefined, reply)
-        })
-      }).exec((err, result) => {
-        if (err) return exits.error(err);
-
-        return exits.success(result);
-      })
-
     } else {
-
-      sails.getDatastore('cache').leaseConnection(function during(redisConnection, proceed) {
-        redisConnection.set(inputs.keyString, inputs.value, (err, reply) => {
-          if (err) return proceed(err);
-
-          return proceed(undefined, reply)
-        })
-      }).exec((err, result) => {
-        if (err) return exits.error(err);
-
-        return exits.success(result);
-      })
+      return exits.success(sails.cache[inputs.keyString]);
     }
 
   }
