f7056dfd12a6952a48c7bdff476e9966142256a2
Niek Candaele <niekcandaele@gmail.com>
2018-11-26 15:00:14 +0100

    Add basic frontend layout for gimme

---
+++ api/models/gimmeItem.js
@@ -0,0 +1,52 @@
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    type: {
+      required: true,
+      type: 'string',
+      isIn: ["item", "command", "entity"]
+    },
+
+    itemName: {
+      type: 'string',
+      minLength: 1
+    },
+
+    friendlyName: {
+      type: 'string',
+      required: true,
+      minLength: 1,
+      maxLength : 100
+    },
+
+    command: {
+      type: 'string',
+      minLength: 1
+    },
+
+    entity: {
+      type: 'number',
+      min: 1
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    server: {
+      model: "SdtdServer"
+    },
+
+  },
+
+};
