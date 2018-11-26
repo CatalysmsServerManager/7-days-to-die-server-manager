25d521ae517da67d87ebe2ea77330e3a5d5ff1f3
Niek Candaele <niekcandaele@gmail.com>
2018-11-26 17:12:31 +0100

    Implement setting cost of gimme

--- api/controllers/SdtdServer/economy/set-cost.js
+++ api/controllers/SdtdServer/economy/set-cost.js
@@ -1,52 +1,53 @@
 module.exports = {
 
-    friendlyName: 'Set cost',
+  friendlyName: 'Set cost',
 
-    description: 'Set the cost of a certain action',
+  description: 'Set the cost of a certain action',
 
-    inputs: {
-        serverId: {
-            type: 'number',
-            required: true
-        },
-
-        costTypeToSet: {
-            type: 'string',
-            required: true,
-            isIn: ['costToTeleport', 'costToSetTeleport', 'costToMakeTeleportPublic']
-        },
-
-        newCost: {
-            type: 'number',
-            required: true,
-            min: 0
-        }
+  inputs: {
+    serverId: {
+      type: 'number',
+      required: true
     },
 
-    exits: {
-        success: {
-        },
+    costTypeToSet: {
+      type: 'string',
+      required: true,
+      isIn: ['costToTeleport', 'costToSetTeleport', 'costToMakeTeleportPublic', 'costToUseGimme']
     },
 
-
-    fn: async function (inputs, exits) {
-
-        try {
-            let updateObject = new Object();
-
-            updateObject[inputs.costTypeToSet] = inputs.newCost;
-            await SdtdConfig.update({server: inputs.serverId}, updateObject);
-            await HistoricalInfo.create({
-                type: 'economy',
-                economyAction: 'config',
-                server: inputs.serverId,
-                message: `set ${inputs.costTypeToSet} to ${inputs.newCost}`
-            });
-            return exits.success();
-        } catch (error) {
-            sails.log.error(`API - Sdtdserver:set-cost - ${error}`);
-            return exits.error(error);
-        }
-
+    newCost: {
+      type: 'number',
+      required: true,
+      min: 0
     }
+  },
+
+  exits: {
+    success: {},
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    try {
+      let updateObject = new Object();
+
+      updateObject[inputs.costTypeToSet] = inputs.newCost;
+      await SdtdConfig.update({
+        server: inputs.serverId
+      }, updateObject);
+      await HistoricalInfo.create({
+        type: 'economy',
+        economyAction: 'config',
+        server: inputs.serverId,
+        message: `set ${inputs.costTypeToSet} to ${inputs.newCost}`
+      });
+      return exits.success();
+    } catch (error) {
+      sails.log.error(`API - Sdtdserver:set-cost - ${error}`);
+      return exits.error(error);
+    }
+
+  }
 };
