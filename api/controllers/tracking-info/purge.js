41792c03aa0540bfca08b31fa6dac0611756c7c8
Niek Candaele <niekcandaele@gmail.com>
2018-11-28 19:46:46 +0100

    Remove .fetch() call when purging tracking data

--- api/controllers/tracking-info/purge.js
+++ api/controllers/tracking-info/purge.js
@@ -1,38 +1,45 @@
 module.exports = {
 
 
-    friendlyName: 'Purge tracking info',
+  friendlyName: 'Purge tracking info',
 
 
-    description: '',
+  description: '',
 
 
-    inputs: {
+  inputs: {
 
-        serverId: {
-            type: 'number',
-            required: true,
-            custom: async (valueToCheck) => {
-                let foundServer = await SdtdServer.findOne(valueToCheck);
-                return foundServer
-            },
-        },
-
+    serverId: {
+      type: 'number',
+      required: true,
+      custom: async (valueToCheck) => {
+        let foundServer = await SdtdServer.findOne(valueToCheck);
+        return foundServer
+      },
     },
 
-
-    exits: {
-    },
+  },
 
 
-    fn: async function (inputs, exits) {
+  exits: {},
 
-        let deletedInfo = await TrackingInfo.destroy({server: inputs.serverId}).fetch();
-        sails.log.info(`Deleted ${deletedInfo.length} records of player tracking data for server ${inputs.serverId}`);
 
-        return exits.success(deletedInfo);
+  fn: async function (inputs, exits) {
 
-    }
+    let totalRecords = await TrackingInfo.count({
+      server: inputs.serverId
+    });
+
+    await TrackingInfo.destroy({
+      server: inputs.serverId
+    });
+
+
+    sails.log.info(`Deleted ${totalRecords} records of player tracking data for server ${inputs.serverId}`);
+
+    return exits.success(deletedInfo);
+
+  }
 
 
 };
