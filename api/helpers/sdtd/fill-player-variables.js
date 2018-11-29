9e0a570ddb0e1ce91399c823b767c38c49ec58ee
Niek Candaele <niekcandaele@gmail.com>
2018-11-29 18:14:09 +0100

    add posX, posY and posZ variables

--- api/helpers/sdtd/fill-player-variables.js
+++ api/helpers/sdtd/fill-player-variables.js
@@ -5,7 +5,7 @@ module.exports = {
 
   description: 'Fills player variables into a command string. Variables should be denoted as ${variable} in the string',
 
-  supportedVariables: ["steamId", "entityId", "playerName", "balance"],
+  supportedVariables: ["steamId", "entityId", "playerName", "balance", "posX", 'posY', 'posZ'],
 
 
   inputs: {
@@ -19,7 +19,7 @@ module.exports = {
       type: 'json',
       required: true,
       custom: (value) => {
-        if (!value.steamId || !value.entityId || !value.name || value.currency === undefined) {
+        if (!value.steamId || !value.entityId || !value.name || value.currency === undefined || value.positionX === undefined || value.positionY === undefined || value.positionZ === undefined) {
           return false;
         } else {
           return true;
@@ -47,6 +47,10 @@ module.exports = {
     inputs.command = replaceAllInString(inputs.command, "${entityId}", inputs.player.entityId);
     inputs.command = replaceAllInString(inputs.command, "${playerName}", inputs.player.name);
     inputs.command = replaceAllInString(inputs.command, "${balance}", inputs.player.currency);
+
+    inputs.command = replaceAllInString(inputs.command, "${posX}", inputs.player.positionX);
+    inputs.command = replaceAllInString(inputs.command, "${posY}", inputs.player.positionY);
+    inputs.command = replaceAllInString(inputs.command, "${posZ}", inputs.player.positionZ);
     return exits.success(inputs.command);
 
   }
