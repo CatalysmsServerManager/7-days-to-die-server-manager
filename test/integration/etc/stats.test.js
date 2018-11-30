48475f46aed9bbebaa4b754d768fed2e05d07426
Niek Candaele <niekcandaele@gmail.com>
2018-11-30 10:34:53 +0100

    Add test case for GET /api/stats

---
+++ test/integration/etc/stats.test.js
@@ -0,0 +1,17 @@
+var supertest = require('supertest');
+var expect = require("chai").expect;
+
+
+
+  describe('GET /api/stats', function() {
+    it('should return an array', function () {
+      return supertest(sails.hooks.http.app)
+      .get('/api/stats')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(response => {
+        expect(response.body).to.be.an('array');
+      })
+    });
+  });
+
