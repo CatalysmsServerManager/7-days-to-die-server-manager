8f44649a6c77dc674bd19b1c0ebb4afee6f59c06
Niek Candaele <niekcandaele@gmail.com>
2018-12-13 02:33:59 +0100

    Fix undefined player id param

--- api/helpers/sdtd/get-player-role.js
+++ api/helpers/sdtd/get-player-role.js
@@ -42,7 +42,7 @@ module.exports = {
     }
 
     if (_.isUndefined(foundRole)) {
-      foundRole = await sails.helpers.roles.getDefaultRole(inputs.serverId);
+      foundRole = await sails.helpers.roles.getDefaultRole(player.server);
     }
 
     //sails.log.verbose(`Found role ${foundRole.name} for player ${player.name}`)
