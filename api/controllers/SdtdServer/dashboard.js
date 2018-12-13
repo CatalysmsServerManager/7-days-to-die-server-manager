ff93195518e60ceb21a399fd8a415779e1e7a955
Niek Candaele <niekcandaele@gmail.com>
2018-12-13 15:00:27 +0100

    Fix issue when loading dashboard page & server is offline

--- api/controllers/SdtdServer/dashboard.js
+++ api/controllers/SdtdServer/dashboard.js
@@ -21,6 +21,9 @@ module.exports = {
       description: 'No server with the specified ID was found in the database.',
       responseType: 'notFound'
     },
+    badRequest: {
+      responseType: 'badRequest'
+    },
 
   },
 
@@ -41,30 +44,36 @@ module.exports = {
       sdtdServerInfo = await sails.helpers.loadSdtdserverInfo(inputs.serverId)
         .tolerate('unauthorized', (error) => {
           sails.log.warn(`VIEW - SdtdServer:dashboard - unauthorized for server cannot load serverInfo ${inputs.serverId}`)
-          return undefined
+          return exits.badRequest(error);
         })
         .tolerate('connectionRefused', error => {
-          return undefined
+          return exits.badRequest(error);
         })
 
       if (!_.isUndefined(sdtdServerInfo)) {
         sdtdServer = sdtdServerInfo;
       }
 
-      let allocsVersion = await sails.helpers.sdtd.checkModVersion('Mod Allocs MapRendering and Webinterface', sdtdServer.id);
-      const cpmVersion = await sails.helpers.sdtd.checkCpmVersion(sdtdServer.id);
-      // let donatorRole = await sails.helpers.meta.checkDonatorStatus.with({ serverId: sdtdServer.id });
+      let allocsVersion = 0;
+      let cpmVersion = 0;
+      try {
+        allocsVersion = await sails.helpers.sdtd.checkModVersion('Mod Allocs MapRendering and Webinterface', sdtdServer.id);
+        cpmVersion = await sails.helpers.sdtd.checkCpmVersion(sdtdServer.id);
+      } catch (error) {
+        sails.log.warn(`VIEW - SdtdServer:dashboard - Could not load mod info ${error}`);
+      }
+
       let userRole = await sails.helpers.roles.getUserRole(this.req.session.user.id, sdtdServer.id);
 
       let allocsObj = {
         supportedAllocs: sails.config.custom.currentAllocs,
         installedAllocs: allocsVersion
-      }
+      };
 
       let cpmObj = {
         supportedCpm: sails.config.custom.currentCpm,
         installedCpm: cpmVersion
-      }
+      };
 
       sails.log.info(`VIEW - SdtdServer:dashboard - Showing dashboard for ${sdtdServer.name} to user ${this.req.session.userId}`);
       return exits.success({
@@ -73,11 +82,10 @@ module.exports = {
         cpmVersion: cpmObj,
         userRole: userRole,
         owner: sdtdServer.owner === parseInt(this.req.session.user.id) ? true : false
-        //   donator: donatorRole
       });
     } catch (error) {
       sails.log.error(`VIEW - SdtdServer:dashboard - ${error}`);
-      throw 'notFound';
+      throw 'badRequest';
     }
 
 
