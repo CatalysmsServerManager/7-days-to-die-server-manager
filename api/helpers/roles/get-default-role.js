47ad20dbc98201a37deeeb6a1b4a05e388d80862
Niek Candaele <niekcandaele@gmail.com>
2018-12-13 02:20:26 +0100

    Fix issue with new helper

--- api/helpers/roles/get-default-role.js
+++ api/helpers/roles/get-default-role.js
@@ -1,68 +1,69 @@
 module.exports = {
 
 
-    friendlyName: 'Check permission',
+  friendlyName: 'Check permission',
 
 
-    description: '',
+  description: '',
 
 
-    inputs: {
+  inputs: {
 
-      serverId: {
-        type: 'number',
-        required: true
-      },
-
+    serverId: {
+      type: 'number',
+      required: true
     },
 
-
-    exits: {
-
-      invalidInput: {
-        description: "Invalid input given to this helper."
-      }
-
-    },
+  },
 
 
-    fn: async function (inputs, exits) {
+  exits: {
 
-      let roles = await Role.find({
-        server: serverId
-      });
-      // Check if server has a role set as default
-      let defaultRole = roles.filter(role => role.isDefault)[0];
-      if (defaultRole.length > 0) {
-        return defaultRole[0];
-      }
-
-      // If we find no default role for a server, we default to highest level role.
-      let foundRole = await Role.find({
-        where: {
-          server: serverId
-        },
-        sort: 'level DESC',
-        limit: 1
-      });
-
-      // If we still can't find a role, it's likely because the server has none configured. In this case we will create a default role.
-      if (!foundRole[0]) {
-        let amountOfRoles = await Role.count({
-          server: serverId
-        });
-
-        sails.log.warn(`Detected ${amountOfRoles} roles for server ${serverId}. Creating a default one`);
-        if (amountOfRoles === 0) {
-          let createdRole = await Role.create({
-            server: serverId,
-            name: "Player",
-            level: "2000",
-          }).fetch();
-          return createdRole;
-        }
-      }
-
-      return foundRole[0];
-
+    invalidInput: {
+      description: "Invalid input given to this helper."
     }
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    let roles = await Role.find({
+      server: inputs.serverId
+    });
+    // Check if server has a role set as default
+    let defaultRole = roles.filter(role => role.isDefault)[0];
+    if (defaultRole) {
+      return exits.success(defaultRole);
+    }
+
+    // If we find no default role for a server, we default to highest level role.
+    let foundRole = await Role.find({
+      where: {
+        server: inputs.serverId
+      },
+      sort: 'level DESC',
+      limit: 1
+    });
+
+    // If we still can't find a role, it's likely because the server has none configured. In this case we will create a default role.
+    if (!foundRole[0]) {
+      let amountOfRoles = await Role.count({
+        server: inputs.serverId
+      });
+
+      sails.log.warn(`Detected ${amountOfRoles} roles for server ${inputs.serverId}. Creating a default one`);
+      if (amountOfRoles === 0) {
+        let createdRole = await Role.create({
+          server: inputs.serverId,
+          name: "Player",
+          level: "2000",
+        }).fetch();
+        return exits.success(createdRole)
+      }
+    }
+
+    return exits.success(foundRole[0]);
+
+  }
+}
