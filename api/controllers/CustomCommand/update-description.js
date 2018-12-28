1e3356c2e482728e07cc8e41028a926222b262dd
Niek Candaele <niekcandaele@gmail.com>
2018-12-28 22:15:39 +0100

    Add description field to custom commands

---
+++ api/controllers/CustomCommand/update-description.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+
+    friendlyName: 'Update description',
+  
+  
+    description: '',
+  
+  
+    inputs: {
+      commandId: {
+        type: 'number',
+        required: true
+      },
+  
+      description: {
+        type: 'string',
+        minLength: 1,
+        maxLenght: 500,
+        required: true
+      },
+  
+    },
+  
+  
+    exits: {
+      success: {}
+    },
+  
+  
+    fn: async function (inputs, exits) {
+  
+      try {
+        await CustomCommand.update({
+          id: inputs.commandId,
+        }, {
+          description: inputs.description
+        })
+        return exits.success();
+  
+      } catch (error) {
+        sails.log.error(error);
+      }
+  
+    }
+  
+  
+  };
+  
