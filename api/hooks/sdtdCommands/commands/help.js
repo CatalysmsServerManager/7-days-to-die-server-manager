53f8459cf808b7ae5427ef8476e7533672d90807
Niek Candaele <niekcandaele@gmail.com>
2018-12-29 18:29:17 +0100

    Add custom commands to help output

--- api/hooks/sdtdCommands/commands/help.js
+++ api/hooks/sdtdCommands/commands/help.js
@@ -2,58 +2,77 @@ let SdtdCommand = require('../command.js');
 const sevenDays = require('machinepack-7daystodiewebapi');
 
 class help extends SdtdCommand {
-    constructor(serverId) {
-        super(serverId, {
-            name: 'help',
-            description: "Get some help"
-        });
-        this.serverId = serverId;
+  constructor(serverId) {
+    super(serverId, {
+      name: 'help',
+      description: "Get some help"
+    });
+    this.serverId = serverId;
+  }
+
+  async isEnabled(chatMessage, player, server, args) {
+    return true;
+  }
+
+  async run(chatMessage, player, server, args) {
+
+    if (player.role) {
+      player.role = await Role.findOne({
+        id: player.role
+      });
+    } else {
+      player.role = await sails.helpers.roles.getDefaultRole(server.id);
     }
 
-    async isEnabled(chatMessage, player, server, args) {
-        return true
-    }
+    if (args[0]) {
+      let commandToGetHelp = await this.commandHandler.findCommandToExecute(args[0]);
 
-    async run(chatMessage, player, server, args) {
+      if (commandToGetHelp) {
 
-        if (args[0]) {
-            let commandToGetHelp = await this.commandHandler.findCommandToExecute(args[0]);
+        let commandEnabled = await commandToGetHelp.isEnabled(chatMessage, player, server, args);
 
-            if (commandToGetHelp) {
-
-                let commandEnabled = await commandToGetHelp.isEnabled(chatMessage, player, server, args);
-
-                if (!commandEnabled) {
-                    return await chatMessage.reply(`This command is not currently enabled! Ask a server admin to enable it.`)
-                }                
-
-                await chatMessage.reply(`${commandToGetHelp.name} - ${commandToGetHelp.description}`);
-                await chatMessage.reply(`${commandToGetHelp.extendedDescription}`);
-                await chatMessage.reply(`Aliases: ${commandToGetHelp.aliases}`)
-            } else {
-                await chatMessage.reply(`Unknown command! Use help without argument to see a full list`)
-            }
-            return
+        if (!commandEnabled) {
+          return await chatMessage.reply(`This command is not currently enabled! Ask a server admin to enable it.`);
         }
 
-
-        await chatMessage.reply("Enabled commands:");
-
-        let commandsArr = this.commandHandler.commands.values()
-
-        for (const command of commandsArr) {
-
-            let commandEnabled = await command.isEnabled(chatMessage, player, server, args);
-
-            if (commandEnabled) {
-                await chatMessage.reply(`${command.name} - ${command.description}`)
-            }
-        }
-
-        await chatMessage.reply(`For more info see: https://confluence.catalysm.net/x/-wAR`)
-
-        return
+        await chatMessage.reply(`${commandToGetHelp.name} - ${commandToGetHelp.description}`);
+        await chatMessage.reply(`${commandToGetHelp.extendedDescription}`);
+        await chatMessage.reply(`Aliases: ${commandToGetHelp.aliases}`)
+      } else {
+        await chatMessage.reply(`Unknown command! Use help without argument to see a full list`);
+      }
+      return;
     }
+
+    const commandsArr = this.commandHandler.commands.values();
+    const customCommands = await CustomCommand.find({
+      where: {
+        server: server.id,
+        level: {
+          '>=': player.role.level
+        }
+      }
+    });
+    await chatMessage.reply("Enabled commands:");
+
+    for (const command of commandsArr) {
+
+      let commandEnabled = await command.isEnabled(chatMessage, player, server, args);
+
+      if (commandEnabled) {
+        await chatMessage.reply(`${command.name} - ${command.description}`)
+      }
+    }
+
+    for (const command of customCommands) {
+
+      await chatMessage.reply(`${command.name} - ${command.description}`);
+    }
+
+    await chatMessage.reply(`For more info see: https://confluence.catalysm.net/x/-wAR`)
+
+    return;
+  }
 }
 
 module.exports = help;
