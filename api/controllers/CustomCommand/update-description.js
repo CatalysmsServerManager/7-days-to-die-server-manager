0f4c5cd02d56b9f357bfda8c9fefff693cd83ae8
Niek Candaele <niekcandaele@gmail.com>
2018-12-29 18:29:01 +0100

    Implement editing description of custom commands

--- api/controllers/CustomCommand/update-description.js
+++ api/controllers/CustomCommand/update-description.js
@@ -1,49 +1,48 @@
 module.exports = {
 
 
-    friendlyName: 'Update description',
-  
-  
-    description: '',
-  
-  
-    inputs: {
-      commandId: {
-        type: 'number',
-        required: true
-      },
-  
-      description: {
-        type: 'string',
-        minLength: 1,
-        maxLenght: 500,
-        required: true
-      },
-  
+  friendlyName: 'Update description',
+
+
+  description: '',
+
+
+  inputs: {
+    commandId: {
+      type: 'number',
+      required: true
     },
-  
-  
-    exits: {
-      success: {}
+
+    description: {
+      type: 'string',
+      minLength: 1,
+      maxLenght: 500,
+      required: true
     },
-  
-  
-    fn: async function (inputs, exits) {
-  
-      try {
-        await CustomCommand.update({
-          id: inputs.commandId,
-        }, {
-          description: inputs.description
-        })
-        return exits.success();
-  
-      } catch (error) {
-        sails.log.error(error);
-      }
-  
+
+  },
+
+
+  exits: {
+    success: {}
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    try {
+      await CustomCommand.update({
+        id: inputs.commandId,
+      }, {
+        description: inputs.description
+      });
+      return exits.success();
+
+    } catch (error) {
+      sails.log.error(error);
     }
-  
-  
-  };
-  
+
+  }
+
+
+};
