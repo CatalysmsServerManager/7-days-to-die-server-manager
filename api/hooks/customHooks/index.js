95249369c70f27dc867fe0a8ea16270e4bf99c53
Niek Candaele <niekcandaele@gmail.com>
2019-03-14 12:47:52 +0100

    Implement logLine event in custom hooks

--- api/hooks/customHooks/index.js
+++ api/hooks/customHooks/index.js
@@ -4,6 +4,8 @@ module.exports = function defineCustomHooksHook(sails) {
 
   return {
 
+    logLineHooks: new Map(),
+
     /**
      * Runs when a Sails app loads/lifts.
      *
@@ -18,6 +20,16 @@ module.exports = function defineCustomHooksHook(sails) {
         let enabledServers = await SdtdConfig.find({
           loggingEnabled: true
         });
+
+        let logLineHooks = await CustomHook.find({
+          event: 'logLine'
+        });
+
+        let groupedLogLinesByServer = _.groupBy(logLineHooks, 'server');
+
+        for (const server of Object.keys(groupedLogLinesByServer)) {
+          this.logLineHooks.set(server, groupedLogLinesByServer[server]);
+        }
 
         for (let config of enabledServers) {
           await this.start(config.server)
@@ -34,14 +46,24 @@ module.exports = function defineCustomHooksHook(sails) {
       let loggingObject = sails.hooks.sdtdlogs.getLoggingObject(serverId);
 
       for (const eventType of sails.config.custom.supportedHooks) {
+
         loggingObject.on(eventType, async eventData => {
+
+          if (eventType === 'logLine') {
+            let serverLogLineHooks = this.logLineHooks.get(String(serverId));
+            for (const serverLogLineHook of serverLogLineHooks) {
+              await executeLogLineHook(eventData, serverLogLineHook, serverId);
+            }
+            return;
+          }
+
           let configuredHooks = await CustomHook.find({
             server: serverId,
             event: eventType
           });
 
           for (const hookToExec of configuredHooks) {
-            executeHook(eventData, hookToExec, serverId);
+            await executeHook(eventData, hookToExec, serverId);
           }
         });
       }
@@ -67,4 +89,44 @@ module.exports = function defineCustomHooksHook(sails) {
     }
   }
 
+  async function executeLogLineHook(eventData, hookToExec, serverId) {
+
+    // Ignore log messages created by the web API. This is the prevent spam
+    if (eventData.msg.includes('WebCommandResult_for_')) {
+      return;
+    }
+
+    let stringFound = checkLogLine(eventData.msg, hookToExec);
+
+    if (stringFound) {
+      let server = await SdtdServer.findOne(serverId);
+
+      let results = await sails.helpers.sdtd.executeCustomCmd(server, hookToExec.commandsToExecute.split(';'), eventData);
+      sails.log.debug(`Executed a custom logLine hook for server ${serverId}`, {
+        hook: hookToExec,
+        event: eventData,
+        results: results
+      });
+    }
+
+    // Checks if the logline matches the searchString or regex
+    function checkLogLine(logLine, hook) {
+
+      let useSearchString;
+
+      if (!_.isEmpty(hook.searchString)) {
+        useSearchString = true;
+      }
+
+      if (!_.isEmpty(hook.regex)) {
+        useSearchString = false;
+      }
+
+      if (useSearchString) {
+        return logLine.includes(hook.searchString);
+      } else {
+        return (new RegExp(hook.regex)).test(logLine);
+      }
+    }
+  }
 };
--- api/models/CustomHook.js
+++ api/models/CustomHook.js
@@ -1,5 +1,56 @@
 module.exports = {
 
+  afterCreate: function (newHook, cb) {
+
+    if (newHook.event === 'logLine') {
+      let currentHooks = sails.hooks.customhooks.logLineHooks.get(newHook.server);
+
+      if (!_.isArray(currentHooks)) {
+        currentHooks = [];
+      };
+      currentHooks.push(newHook);
+
+      sails.hooks.customhooks.logLineHooks.set(String(newHook.server), currentHooks);
+    }
+
+    return cb();
+  },
+
+  afterUpdate: function (newHook, cb) {
+
+    if (newHook.event === 'logLine') {
+      let currentHooks = sails.hooks.customhooks.logLineHooks.get(newHook.server);
+
+      if (!_.isArray(currentHooks)) {
+        currentHooks = [];
+      };
+
+      _.remove(currentHooks, (h) => {
+        return h.id === newHook.id;
+      });
+
+      currentHooks.push(newHook);
+
+      sails.hooks.customhooks.logLineHooks.set(String(newHook.server), currentHooks);
+    }
+
+    return cb();
+  },
+
+  beforeDestroy: function(deletedHook, cb) {
+
+    if (deletedHook.event === 'logLine') {
+      let currentHooks = sails.hooks.customhooks.logLineHooks.get(deletedHook.server);
+      _.remove(currentHooks, (h) => {
+        return h.id === deletedHook.id;
+      });
+
+      sails.hooks.customhooks.logLineHooks.set(String(deletedHook.server), currentHooks);
+    }
+
+    return cb();
+  },
+
   attributes: {
 
     // What event will trigger this hook
@@ -15,7 +66,7 @@ module.exports = {
     },
 
     searchString: {
-      type:'string'
+      type: 'string'
     },
 
     regex: {
