c95cb55e3e095d26c42550f15e61f2fbd127f017
Niek Candaele <niekcandaele@gmail.com>
2019-03-14 12:47:36 +0100

    API routes for new config of custom hooks

--- api/controllers/CustomHook/add-custom-hook.js
+++ api/controllers/CustomHook/add-custom-hook.js
@@ -23,23 +23,46 @@ module.exports = {
       required: true,
       type: 'string',
       isIn: sails.config.custom.supportedHooks
-    }
+    },
+
+    searchString: {
+      type: 'string',
+      minLength: 5
+    },
+
+    regex: {
+      type: 'string',
+    },
 
 
   },
 
 
   exits: {
-
+    badInput: {
+      responseType: 'badRequest'
+    }
   },
 
 
   fn: async function (inputs, exits) {
 
+    if (inputs.event === 'logLine') {
+      if (_.isEmpty(inputs.regex) && _.isEmpty(inputs.searchString)) {
+        return exits.badInput('When using "logLine", you must define either a search string or a regex.');
+      }
+
+      if (!_.isEmpty(inputs.regex) && !_.isEmpty(inputs.searchString)) {
+        return exits.badInput('You can use a regex OR a search string. Not both.');
+      }
+    }
+
     let result = await CustomHook.create({
       server: inputs.serverId,
       commandsToExecute: inputs.commandsToExecute,
-      event: inputs.event
+      event: inputs.event,
+      searchString: inputs.searchString,
+      regex: inputs.regex
     }).fetch();
     return exits.success(result);
 
