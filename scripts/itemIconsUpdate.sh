e430ffa30191a695f4f008350499141487204b27
Niek Candaele <niekcandaele@gmail.com>
2019-03-20 13:49:58 +0100

    Update itemIcons:update script

--- scripts/itemIconsUpdate.sh
+++ scripts/itemIconsUpdate.sh
@@ -5,6 +5,7 @@ rm -rf sdtdIcons/
 LOCATION=$(curl -s https://api.github.com/repos/CatalysmsServerManager/7dtd-icons/releases/latest \
 | grep "tag_name" \
 | awk '{print "https://github.com/CatalysmsServerManager/7dtd-icons/releases/download/" substr($2, 2, length($2)-3) "/sdtdIcons.tar.gz"}') \
-; curl -L -o icons.tar.gz $LOCATION \
+; echo "Downloading icons from $LOCATION" \
+; curl -L -o icons.tar.gz "$LOCATION" \
 ; tar -xzf icons.tar.gz sdtdIcons  \
 ; rm icons.tar.gz \
