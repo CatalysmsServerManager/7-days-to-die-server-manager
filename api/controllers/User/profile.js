fe1288cc7601d91237f24ce9c090f8d55570a085
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2019-07-19 12:13:32 +0000

    Display role info on user profile page

--- api/controllers/User/profile.js
+++ api/controllers/User/profile.js
@@ -1,53 +1,57 @@
 module.exports = {
 
-    friendlyName: 'Get user profile',
+  friendlyName: 'Get user profile',
 
-    description: 'Serves user profile view',
+  description: 'Serves user profile view',
 
-    inputs: {
-        userId: {
-            description: 'The ID of the user to look up.',
-            type: 'number',
-            required: true
-        }
-    },
-
-    exits: {
-        success: {
-            responseType: 'view',
-            viewTemplatePath: 'user/profile'
-        },
-        notFound: {
-            description: 'No user with the specified ID was found in the database.',
-            responseType: 'notFound'
-        }
-    },
-
-    /**
-     * @memberof User
-     * @name profile
-     * @description Serves user profile view
-     * @param {number} userId
-     */
-
-    fn: async function (inputs, exits) {
-        sails.log.debug(`API - User:profile - Getting profile for user ${inputs.userId}`);
-
-        try {
-            let user = await User.findOne(inputs.userId);
-            let servers = await SdtdServer.find({ owner: inputs.userId });
-            let players = await Player.find({ steamId: user.steamId }).populate('tickets').populate('server');
-
-            return exits.success({
-                user: user,
-                servers: servers,
-                players: players
-            })
-
-        } catch (error) {
-            sails.log.error(`API - SdtdServer:profile - ${error}`);
-            return exits.error(error);
-        }
-
+  inputs: {
+    userId: {
+      description: 'The ID of the user to look up.',
+      type: 'number',
+      required: true
     }
+  },
+
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'user/profile'
+    },
+    notFound: {
+      description: 'No user with the specified ID was found in the database.',
+      responseType: 'notFound'
+    }
+  },
+
+  /**
+   * @memberof User
+   * @name profile
+   * @description Serves user profile view
+   * @param {number} userId
+   */
+
+  fn: async function (inputs, exits) {
+    sails.log.debug(`API - User:profile - Getting profile for user ${inputs.userId}`);
+
+    try {
+      let user = await User.findOne(inputs.userId);
+      let servers = await SdtdServer.find({
+        owner: inputs.userId
+      });
+      let players = await Player.find({
+        steamId: user.steamId
+      }).populate('tickets').populate('server').populate('role');
+
+      return exits.success({
+        user: user,
+        servers: servers,
+        players: players
+      })
+
+    } catch (error) {
+      sails.log.error(`API - SdtdServer:profile - ${error}`);
+      return exits.error(error);
+    }
+
+  }
 };
