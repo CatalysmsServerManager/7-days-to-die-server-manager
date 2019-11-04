ab5865cd612b930321c966383b6ed87bef09ad35
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2019-11-04 13:07:17 +0000

    Control log collection count & interval with env vars

--- api/hooks/sdtdLogs/LoggingObject.js
+++ api/hooks/sdtdLogs/LoggingObject.js
@@ -1,69 +1,84 @@
-const SdtdApi = require('7daystodie-api-wrapper');
-const EventEmitter = require('events');
-const Bull = require('bull');
-const logProcessor = require('./logProcessor');
-const enrichData = require('./enrichEventData');
+const SdtdApi = require("7daystodie-api-wrapper");
+const EventEmitter = require("events");
+const Bull = require("bull");
+const logProcessor = require("./logProcessor");
+const enrichData = require("./enrichEventData");
 
-const {
-  inspect
-} = require('util');
+const { inspect } = require("util");
 
 const defaultIntervalMs = 2000;
 const slowModeIntervalms = 300000;
 
 class LoggingObject extends EventEmitter {
-
-  constructor(ip, port, authName, authToken, serverId, intervalTime = defaultIntervalMs) {
+  constructor(
+    ip,
+    port,
+    authName,
+    authToken,
+    serverId,
+    intervalTime = defaultIntervalMs
+  ) {
     super();
     this.server = {
       id: serverId,
       ip: ip,
       port: port,
       adminUser: authName,
-      adminToken: authToken,
+      adminToken: authToken
     };
 
     this.active = true;
-    this.queue = new Bull(`sdtdserver:${serverId}:logs`, process.env.REDISSTRING);
+    this.queue = new Bull(
+      `sdtdserver:${serverId}:logs`,
+      process.env.REDISSTRING
+    );
     this.intervalTime = intervalTime;
     this.requestInterval;
     this.failed = false;
     // Keep track of how many times we receive an empty response.
-    // If we get too many empty responses, we force a recheck of lastLogLine 
+    // If we get too many empty responses, we force a recheck of lastLogLine
     this.emptyResponses = 0;
     // Set this to true to view detailed info about logs for a server. (protip: use discord bot eval command to set this to true in production instances)
     this.debug = false;
     this.queue.process(logProcessor);
     this.init();
-    this.queue.on('completed', (job, result) => this.handleCompletedJob(job, result, this));
-    this.queue.on('failed', (job, err) => this.handleFailedJob(job, err, this));
-    this.queue.on('error', this.handleError);
-    this.queue.on('cleaned', function(jobs, type) {
-      sails.log.debug('Cleaned %s %s jobs', jobs.length, type);
+    this.queue.on("completed", (job, result) =>
+      this.handleCompletedJob(job, result, this)
+    );
+    this.queue.on("failed", (job, err) => this.handleFailedJob(job, err, this));
+    this.queue.on("error", this.handleError);
+    this.queue.on("cleaned", function(jobs, type) {
+      sails.log.debug("Cleaned %s %s jobs", jobs.length, type);
     });
   }
 
-  async init(ms = 3000) {
+  async init(ms = parseInt(process.env.CSMM_LOG_CHECK_INTERVAL)) {
+    if (!ms) {
+      ms = 3000;
+    }
 
     // Make sure there are no lingering jobs
     await this.stop();
-    
+
     try {
       await this.setLastLogLine();
     } catch (error) {
       // Fail silently
     }
-    return await this.queue.add({
-      server: this.server,
-      lastLogLine: this.lastLogLine
-    }, {
-      repeat: {
-        every: ms,
+    return await this.queue.add(
+      {
+        server: this.server,
+        lastLogLine: this.lastLogLine
       },
-      removeOnFail: 50,
-      removeOnComplete: 200,
-      timeout: 5000,
-    });
+      {
+        repeat: {
+          every: ms
+        },
+        removeOnFail: 50,
+        removeOnComplete: 200,
+        timeout: 5000
+      }
+    );
   }
 
   async reload() {
@@ -73,7 +88,7 @@ class LoggingObject extends EventEmitter {
   }
 
   async handleError(error) {
-    sails.log.error(inspect(error))
+    sails.log.error(inspect(error));
   }
 
   async handleFailedJob(job, err, loggingObject) {
@@ -84,9 +99,8 @@ class LoggingObject extends EventEmitter {
   }
 
   async handleCompletedJob(job, result, loggingObject) {
-
     if (result.logs.length === 0) {
-      this.emptyResponses++
+      this.emptyResponses++;
     }
 
     if (this.emptyResponses > 5) {
@@ -94,7 +108,7 @@ class LoggingObject extends EventEmitter {
     }
 
     for (const newLog of result.logs) {
-      let enrichedLog = newLog
+      let enrichedLog = newLog;
       if (newLog.type !== "logLine") {
         enrichedLog = await enrichData(newLog);
       }
@@ -118,12 +132,11 @@ class LoggingObject extends EventEmitter {
     return;
   }
 
-
   async stop() {
     await this.queue.empty();
-    await this.queue.clean(0, 'completed');
-    await this.queue.clean(0, 'failed');
-    return
+    await this.queue.clean(0, "completed");
+    await this.queue.clean(0, "failed");
+    return;
   }
 
   _toggleDebug() {
@@ -131,42 +144,56 @@ class LoggingObject extends EventEmitter {
   }
 
   async setFailedToZero() {
-    await sails.helpers.redis.set(`sdtdserver:${this.server.id}:sdtdLogs:failedCounter`, 0);
+    await sails.helpers.redis.set(
+      `sdtdserver:${this.server.id}:sdtdLogs:failedCounter`,
+      0
+    );
   }
 
   async setLastLogLine() {
     const webUIUpdate = await SdtdApi.getWebUIUpdates(this.server);
     const lastLogLine = parseInt(webUIUpdate.newlogs) + 1;
-    await sails.helpers.redis.set(`sdtdserver:${this.server.id}:sdtdLogs:lastLogLine`, lastLogLine);
+    await sails.helpers.redis.set(
+      `sdtdserver:${this.server.id}:sdtdLogs:lastLogLine`,
+      lastLogLine
+    );
     this.emptyResponses = 0;
-    return lastLogLine
+    return lastLogLine;
   }
 
   // Called when a request to a server fails for whatever reason
   async _failedHandler() {
     const threeDaysInMs = 1000 * 60 * 60 * 24 * 3;
     this.failed = true;
-    let counter = await sails.helpers.redis.incr(`sdtdserver:${this.server.id}:sdtdLogs:failedCounter`);
-    let lastSuccess = await sails.helpers.redis.get(`sdtdserver:${this.server.id}:sdtdLogs:lastSuccess`);
+    let counter = await sails.helpers.redis.incr(
+      `sdtdserver:${this.server.id}:sdtdLogs:failedCounter`
+    );
+    let lastSuccess = await sails.helpers.redis.get(
+      `sdtdserver:${this.server.id}:sdtdLogs:lastSuccess`
+    );
     lastSuccess = parseInt(lastSuccess);
     if (counter > 100) {
       let prettyLastSuccess = new Date(lastSuccess);
 
       if (!this.slowmode) {
-        sails.log.info(`SdtdLogs - Server ${this.server.id} has failed ${counter} times. Changing interval time. Server was last successful on ${prettyLastSuccess.toLocaleDateString()} ${prettyLastSuccess.toLocaleTimeString()}`);
+        sails.log.info(
+          `SdtdLogs - Server ${
+            this.server.id
+          } has failed ${counter} times. Changing interval time. Server was last successful on ${prettyLastSuccess.toLocaleDateString()} ${prettyLastSuccess.toLocaleTimeString()}`
+        );
         this.slowmode = true;
         await this.stop();
         await this.init(300000);
       }
 
       if (lastSuccess + threeDaysInMs < Date.now()) {
-        sails.log.warn(`SdtdLogs - server ${this.server.id} has not responded in over 3 days, setting to inactive`);
+        sails.log.warn(
+          `SdtdLogs - server ${this.server.id} has not responded in over 3 days, setting to inactive`
+        );
         await sails.helpers.meta.setServerInactive(this.server.id);
       }
     }
-
   }
-
 }
 
-module.exports = LoggingObject
+module.exports = LoggingObject;
--- api/hooks/sdtdLogs/logProcessor.js
+++ api/hooks/sdtdLogs/logProcessor.js
@@ -1,10 +1,10 @@
-const Redis = require('redis');
-const dotenv = require('dotenv');
-const promisify = require('util').promisify
-const SdtdApi = require('7daystodie-api-wrapper');
-const _ = require('lodash');
+const Redis = require("redis");
+const dotenv = require("dotenv");
+const promisify = require("util").promisify;
+const SdtdApi = require("7daystodie-api-wrapper");
+const _ = require("lodash");
 
-const handleLogLine = require('./handleLogLine');
+const handleLogLine = require("./handleLogLine");
 
 /**
  * Set up Redis functionality
@@ -16,10 +16,14 @@ const redisClient = Redis.createClient({
 redisClient.get = promisify(redisClient.get);
 redisClient.set = promisify(redisClient.set);
 
-module.exports = async function (job) {
+module.exports = async function(job) {
   const resultLogs = [];
   // Get latest log line from Redis
-  let lastLogLine = parseInt(await redisClient.get(`sdtdserver:${job.data.server.id}:sdtdLogs:lastLogLine`));
+  let lastLogLine = parseInt(
+    await redisClient.get(
+      `sdtdserver:${job.data.server.id}:sdtdLogs:lastLogLine`
+    )
+  );
 
   // If latest log line is not found, get it from the server
   if (!lastLogLine) {
@@ -27,21 +31,28 @@ module.exports = async function (job) {
     lastLogLine = parseInt(webUIUpdate.newlogs) + 1;
   }
 
+  const count = process.env.CSMM_LOG_COUNT
+    ? parseInt(process.env.CSMM_LOG_COUNT)
+    : 50;
+
   // Get new logs from the server
-  const newLogs = await SdtdApi.getLog(job.data.server, lastLogLine, 50);
+  const newLogs = await SdtdApi.getLog(job.data.server, lastLogLine, count);
 
   // Adjust latest log line based on new logs we got
   lastLogLine = lastLogLine + newLogs.entries.length;
 
   // Set latest log line in Redis
-  await redisClient.set(`sdtdserver:${job.data.server.id}:sdtdLogs:lastLogLine`, lastLogLine);
+  await redisClient.set(
+    `sdtdserver:${job.data.server.id}:sdtdLogs:lastLogLine`,
+    lastLogLine
+  );
 
   // Parse these logs into usable data
   let index = -1;
   _.each(newLogs.entries, async line => {
     index++;
     if (newLogs.entries[index + 1]) {
-      if (newLogs.entries[index + 1].msg.includes('handled by mod')) {
+      if (newLogs.entries[index + 1].msg.includes("handled by mod")) {
         //Message is being handled by a mod, skip to the next line with possibly mod-controlled data
         return;
       }
@@ -55,10 +66,13 @@ module.exports = async function (job) {
   });
 
   // Set last success date in Redis
-  await redisClient.set(`sdtdserver:${job.data.server.id}:sdtdLogs:lastSuccess`, Date.now());
+  await redisClient.set(
+    `sdtdserver:${job.data.server.id}:sdtdLogs:lastSuccess`,
+    Date.now()
+  );
 
   return Promise.resolve({
     lastLogLine,
     logs: resultLogs
   });
-}
+};
