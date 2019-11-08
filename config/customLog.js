5050b17faaebd63eb6a9e36b96ea55134140c6d7
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2019-11-08 13:54:06 +0000

    Configure loglevel via env var

--- config/customLog.js
+++ config/customLog.js
@@ -1,42 +1,57 @@
-const winston = require('winston');
+const winston = require("winston");
 
-const customLogger = new winston.Logger({
-  transports: [
+const logLevel = process.env.CSMM_LOGLEVEL;
+
+if (logLevel !== "debug" && logLevel !== "info") {
+  throw new Error(`Invalid log level given, please select "debug" or "info"`);
+}
+
+const transports = [
+  new winston.transports.File({
+    level: "info",
+    name: "infolog",
+    timestamp: true,
+    humanReadableUnhandledException: false,
+    filename: "./logs/prod.log",
+    tailable: true,
+    maxsize: 10000000,
+    maxFiles: 3,
+    json: false,
+    colorize: false
+  })
+];
+
+if (logLevel === "debug") {
+  transports.push(
     new winston.transports.Console({
-      level: 'debug',
+      level: "debug",
       colorize: true,
       timestamp: true,
       humanReadableUnhandledException: true
-    }),
+    })
+  );
+  transports.push(
     new winston.transports.File({
-      level: 'info',
-      name: 'infolog',
-      timestamp: true,
-      humanReadableUnhandledException: false,
-      filename: './logs/prod.log',
-      tailable: true,
-      maxsize: 10000000,
-      maxFiles: 3,
-      json: false,
-      colorize: false,
-    }),
-    new winston.transports.File({
-      level: 'debug',
-      name: 'debuglog',
+      level: "debug",
+      name: "debuglog",
       timestamp: true,
       humanReadableUnhandledException: true,
-      filename: './logs/debug.log',
+      filename: "./logs/debug.log",
       tailable: true,
       maxsize: 10000000,
       maxFiles: 5,
       json: false,
       colorize: true
     })
-  ]
+  );
+}
+
+const customLogger = new winston.Logger({
+  transports: transports
 });
 
 customLogger.stream = {
-  write: function (message) {
+  write: function(message) {
     customLogger.info(message);
   }
 };
