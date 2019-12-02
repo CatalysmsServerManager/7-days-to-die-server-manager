0dcd01d6fb202ceb53cc3d01e19eb0f267a6b20a
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2019-12-02 11:59:26 +0100

    Remove command validation when importing customs

--- api/controllers/CustomCommand/command-import.js
+++ api/controllers/CustomCommand/command-import.js
@@ -1,133 +1,118 @@
-const validator = require('validator');
-const cronParser = require('cron-parser');
+const validator = require("validator");
+const cronParser = require("cron-parser");
 
 module.exports = {
+  friendlyName: "Cron export",
 
-
-  friendlyName: 'Cron export',
-
-
-  description: '',
-
+  description: "",
 
   inputs: {
     serverId: {
-      type: 'number'
+      type: "number"
     },
     file: {
-      type: 'ref'
-    },
-
+      type: "ref"
+    }
   },
-
 
   exits: {
     success: {},
 
     invalidIds: {
       description: "Must give either listing or server ID",
-      responseType: 'badRequest',
+      responseType: "badRequest",
       statusCode: 400
     },
 
     invalidInput: {
-      responseType: 'badRequest',
+      responseType: "badRequest",
       statusCode: 400
     }
   },
 
-
-  fn: async function (inputs, exits) {
+  fn: async function(inputs, exits) {
     let problems = new Array();
     let server = await SdtdServer.findOne(inputs.serverId);
 
     try {
-      JSON.parse(inputs.file)
+      JSON.parse(inputs.file);
     } catch (error) {
-      return exits.invalidInput(`Malformed JSON - ${error}`)
+      return exits.invalidInput(`Malformed JSON - ${error}`);
     }
 
     let newData = JSON.parse(inputs.file);
-    let allowedCommands = await sails.helpers.sdtd.getAllowedCommands(server);
-
     let donatorRole = await sails.helpers.meta.checkDonatorStatus.with({
       serverId: server.id
     });
-    let maxCustomCommands = sails.config.custom.donorConfig[donatorRole].maxCustomCommands;
+    let maxCustomCommands =
+      sails.config.custom.donorConfig[donatorRole].maxCustomCommands;
 
     if (newData.length >= maxCustomCommands) {
-      problems.push(`Too many commands, you are allowed ${maxCustomCommands}`)
+      problems.push(`Too many commands, you are allowed ${maxCustomCommands}`);
     }
 
-
-
     for (const newCommand of newData) {
-
-      if (newCommand.name.includes(' ')) {
-        problems.push('Name cannot have spaces');
+      if (newCommand.name.includes(" ")) {
+        problems.push("Name cannot have spaces");
       }
 
-      let commandsToExecute = newCommand.commandsToExecute.split(';');
-
-      // Check if we can execute the entered commands
-      commandsToExecute.forEach(command => {
-        let splitMessage = command.split(' ');
-        splitMessage = splitMessage.filter(el => el !== "");
-        let idx = allowedCommands.findIndex(serverCommand => serverCommand == splitMessage[0]);
-        if (idx === -1) {
-          problems.push(`${splitMessage[0]} is not a valid command.`);
-        }
-      });
+      let commandsToExecute = newCommand.commandsToExecute.split(";");
 
       if (!_.isBoolean(newCommand.enabled)) {
-        problems.push(`Enabled must be true or false.`)
+        problems.push(`Enabled must be true or false.`);
       }
 
-      if (!validator.isInt(newCommand.delay + '', {
+      if (
+        !validator.isInt(newCommand.delay + "", {
           min: 0
-        })) {
-        problems.push(`${newCommand.delay} is not a valid delay.`)
+        })
+      ) {
+        problems.push(`${newCommand.delay} is not a valid delay.`);
       }
 
-      if (!validator.isInt(newCommand.timeout + '', {
+      if (
+        !validator.isInt(newCommand.timeout + "", {
           min: 0
-        })) {
-        problems.push(`${newCommand.timeout} is not a valid timeout.`)
+        })
+      ) {
+        problems.push(`${newCommand.timeout} is not a valid timeout.`);
       }
 
-      if (!validator.isInt(newCommand.costToExecute + '', {
+      if (
+        !validator.isInt(newCommand.costToExecute + "", {
           min: 0
-        })) {
-        problems.push(`${newCommand.costToExecute} is not a valid costToExecute.`)
+        })
+      ) {
+        problems.push(
+          `${newCommand.costToExecute} is not a valid costToExecute.`
+        );
       }
-
     }
 
     if (problems.length === 0) {
-
       let currentCommands = await CustomCommand.find({
         server: inputs.serverId
       });
 
       await CustomCommandArgument.destroy({
         command: currentCommands.map(cmd => cmd.id)
-      })
+      });
 
       await CustomCommand.destroy({
         server: inputs.serverId
       });
-      let createdRecords = await CustomCommand.createEach(newData.map(newCommand => {
-        newCommand.server = inputs.serverId
-        return _.omit(newCommand, "arguments")
-      }));
-      sails.log.info(`Imported ${newData.length} commands for server ${inputs.serverId}`);
-      return exits.success()
+      let createdRecords = await CustomCommand.createEach(
+        newData.map(newCommand => {
+          newCommand.server = inputs.serverId;
+          return _.omit(newCommand, "arguments");
+        })
+      );
+      sails.log.info(
+        `Imported ${newData.length} commands for server ${inputs.serverId}`
+      );
+      return exits.success();
     } else {
-      return exits.invalidInput(problems)
+      return exits.invalidInput(problems);
     }
-
-
   }
-
-
 };
