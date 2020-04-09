e33e9de6e0b6e815170269564dd69e37a141e3ba
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-04-09 17:20:42 +0200

    Implement wiping player currency

---
+++ api/controllers/SdtdServer/wipe-currency.js
@@ -0,0 +1,36 @@
+module.exports = {
+
+    friendlyName: 'Wipe currency',
+
+    description: '',
+
+    inputs: {
+        serverId: {
+            description: 'The ID of the server',
+            type: 'number',
+            required: true
+        },
+    },
+
+    exits: {
+
+        badInput: {
+            responseType: 'badRequest'
+        }
+    },
+
+    fn: async function (inputs, exits) {
+
+        let playersToDestroy = await Player.find({
+            server: inputs.serverId
+        });
+
+        let server = await SdtdServer.findOne(inputs.serverId);
+
+        await Player.update({ id: playersToDestroy.map(_ => _.id) }, { currency: 0 })
+
+        sails.log.info(`Set currency of ${playersToDestroy.length} player(s) from server ${server.name} to 0`);
+
+        return exits.success(playersToDestroy);
+    }
+};
