ee1d4ec16c445a6be3b061ae1bc1355040ca0dab
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2020-05-09 20:18:09 +0200

    Docker build needs a proper healthcheck (#84)

---
+++ scripts/healthcheck.js
@@ -0,0 +1,30 @@
+/**
+ * This code can be used to see if a CSMM process is healthy or not
+ * Used in Docker healthcheck
+ */
+
+const http = require("http");
+
+const options = {  
+    host : "localhost",
+    port : "1337",
+    timeout : 2000,
+    path: '/api/stats/'
+};
+
+const request = http.request(options, (res) => {  
+    console.log(`STATUS: ${res.statusCode}`);
+    if (res.statusCode == 200) {
+        process.exit(0);
+    }
+    else {
+        process.exit(1);
+    }
+});
+
+request.on('error', function(err) {  
+    console.log('ERROR');
+    process.exit(1);
+});
+
+request.end();  
