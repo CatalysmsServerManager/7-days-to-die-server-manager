d0caea944b6910f8f15ed78ad56dc199c1981c1f
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2020-05-22 14:20:13 +0200

    Allow workers to handle 25 jobs (#99)

--- worker.js
+++ worker.js
@@ -44,7 +44,7 @@ sails.load(configOverrides, async function (err) {
   sails.log('Running bulls worker');
 
   await Promise.all([
-    sails.helpers.getQueueObject('logs').process(async (job) => {
+    sails.helpers.getQueueObject('logs').process(25, async (job) => {
       sails.log.debug('[Worker] Got a `logs` job', job.data);
       job.data.server = await SdtdServer.findOne(job.data.serverId)
       return logProcessor(job);
