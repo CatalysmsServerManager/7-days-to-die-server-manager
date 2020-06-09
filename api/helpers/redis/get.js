5cb0f0b7e7ce98fde64eb2c9f6a9e885a57749c9
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-06-09 18:32:06 +0200

    Return null instead of undefined when in memory cache get is used

--- api/helpers/redis/get.js
+++ api/helpers/redis/get.js
@@ -40,7 +40,7 @@ module.exports = {
       if (!sails.cache) {
         sails.cache = {};
       }
-      return exits.success(sails.cache[inputs.keyString]);
+      return exits.success(sails.cache[inputs.keyString] ? sails.cache[inputs.keyString] : null);
     }
 
   }
