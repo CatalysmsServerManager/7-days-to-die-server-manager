bf6de9d13e65dc9838b8bdf56de3cf060a5be7fe
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-06-20 09:23:44 +0200

    Refactor redis helpers to use in memory cache

--- api/helpers/redis/get.js
+++ api/helpers/redis/get.js
@@ -23,26 +23,13 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    const datastore = sails.getDatastore('cache');
-    if (datastore.config.adapter === 'sails-redis') {
-      sails.getDatastore('cache').leaseConnection(function during(redisConnection, proceed) {
-        redisConnection.get(inputs.keyString, (err, reply) => {
-          if (err) return proceed(err);
 
-          return proceed(undefined, reply)
-        });
-      }).exec((err, result) => {
-        if (err) return exits.error(err);
-
-        return exits.success(result);
-      });
-    } else {
-      if (!sails.cache) {
-        sails.cache = {};
-      }
-      return exits.success(sails.cache[inputs.keyString] ? sails.cache[inputs.keyString] : null);
+    if (!sails.cache) {
+      sails.cache = {};
     }
 
+    return exits.success(sails.cache[inputs.keyString] ? sails.cache[inputs.keyString] : null);
   }
+
 };
 
--- api/helpers/redis/incr.js
+++ api/helpers/redis/incr.js
@@ -23,21 +23,11 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    const datastore = sails.getDatastore('cache');
-    if (datastore.config.adapter === 'sails-redis') {
-      sails.getDatastore('cache').leaseConnection(function during(redisConnection, proceed) {
-        redisConnection.incr(inputs.keyString, (err, reply) => {
-          if (err) return proceed(err);
 
-          return proceed(undefined, reply)
-        });
-      }).exec((err, result) => {
-        if (err) return exits.error(err);
-
-        return exits.success(result);
-      });
-    } else {
-      return exits.success(sails.cache[inputs.keyString]++);
+    if (!sails.cache[inputs.keyString]) {
+      sails.cache[inputs.keyString] = 0;
     }
+
+    return exits.success(sails.cache[inputs.keyString]++);
   }
 };
