a9a0fdf93522704690db11e7f6ff55fe42f123ee
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-07-02 22:49:05 +0200

    Add a failing test

---
+++ test/integration/controllers/give-item.test.js
@@ -0,0 +1,29 @@
+const supertest = require('supertest');
+const { expect } = require("chai");
+
+describe('/api/player/giveitem', function () {
+    it('returns OK with correct data', async function () {
+        sandbox.stub(sails.helpers.sdtdApi, "executeConsoleCommand").callsFake(async () => {
+            return {
+                result: "Item given"
+            }
+        });
+        sandbox.stub(sails.helpers.sdtd, "checkCpmVersion").callsFake(async () => 7);
+
+        const response = await supertest(sails.hooks.http.app)
+            .post('/api/player/giveitem')
+            .send({
+                playerId: 1,
+                itemName: "something",
+                amount: "1"
+            })
+
+        console.log(response.body);
+
+        expect(response.statusCode).to.equal(200);
+
+    })
+
+});
+
+
