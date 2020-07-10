f1aa0b56484e89c5845f31b4a9e551ad14dce4ad
Gavin Mogan <git@gavinmogan.com>
2020-07-09 22:46:05 -0700

    Make sure we use SdtdServer.getAPIConfig when calling sdtdapi calls, since the keys between manager and api don't match up correctly

--- api/controllers/Player/give-item.js
+++ api/controllers/Player/give-item.js
@@ -61,24 +61,14 @@ module.exports = {
         cmdToExec = `give ${player.entityId} ${inputs.itemName} ${inputs.amount} ${inputs.quality ? inputs.quality : ''}`;
       }
 
-      let response = await sails.helpers.sdtdApi.executeConsoleCommand({
-        ip: server.ip,
-        port: server.webPort,
-        adminUser: server.authName,
-        adminToken: server.authToken
-      }, cmdToExec);
+      let response = await sails.helpers.sdtdApi.executeConsoleCommand(SdtdServer.getAPIConfig(server), cmdToExec);
 
       if (response.result.startsWith('ERR:')) {
         return exits.badRequest(`Error while giving item - ${response.result}`);
       }
 
 
-      await sails.helpers.sdtdApi.executeConsoleCommand({
-        ip: server.ip,
-        port: server.webPort,
-        adminUser: server.authName,
-        adminToken: server.authToken
-      }, `pm ${player.steamId} "CSMM - You have received ${inputs.amount} of ${inputs.itemName}"`);
+      await sails.helpers.sdtdApi.executeConsoleCommand(SdtdServer.getAPIConfig(server), `pm ${player.steamId} "CSMM - You have received ${inputs.amount} of ${inputs.itemName}"`);
 
       sails.log.info(`API - Player:give-item - giving ${inputs.amount} of ${inputs.itemName} to ${inputs.playerId} with quality: ${inputs.quality}`);
       return exits.success();
