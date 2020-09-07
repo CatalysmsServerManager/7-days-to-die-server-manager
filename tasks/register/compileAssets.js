a934ae631dd6f610f81a477934f0ae9003b8dd35
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-09-07 08:14:46 +0200

    fix JST is no longer part of sails grunt close #301

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -7,12 +7,10 @@
  *   https://sailsjs.com/anatomy/tasks/register/compile-assets.js
  *
  */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.registerTask('compileAssets', [
     'clean:dev',
-    'jst:dev',
     'less:dev',
     'copy:dev',
-    'coffee:dev'
   ]);
 };
