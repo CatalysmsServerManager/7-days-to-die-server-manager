949041b68d7a63101a796539de61f1b656b9a95b
Andrew <andrey255@live.com>
2020-10-02 21:30:18 +0300

    Update config/custom.js
    Co-authored-by: Gavin Mogan <github@gavinmogan.com>

--- config/custom.js
+++ config/custom.js
@@ -28,7 +28,7 @@ module.exports.custom = {
   logCount: !Number.isNaN(parseInt(process.env.CSMM_LOG_COUNT)) ? parseInt(process.env.CSMM_LOG_COUNT) : 50,
 
   // Discord bot config
-  botOwners: process.env.DISCORDOWNERIDS ? process.env.DISCORDOWNERIDS.split(',') : undefined,
+  botOwners: process.env.DISCORDOWNERIDS ? process.env.DISCORDOWNERIDS.split(',') : [],
   botToken: process.env.DISCORDBOTTOKEN,
   botEmbedLink: `${process.env.CSMM_HOSTNAME}`,
   botEmbedTitle: 'CSMM',
