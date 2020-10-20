576a6fd5863aff0d277f245cb4fc5ee52603178c
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-10-20 22:06:25 +0200

    Clean up Redis pubsub tests

--- test/unit/helpers/redis/pubsub.test.js
+++ test/unit/helpers/redis/pubsub.test.js
@@ -9,12 +9,14 @@ describe('Redis Pub/Sub', () => {
   it('Receives a published message', async () => {
     const subscriber = await sails.helpers.redis.subscribe('test channel');
 
+    const p = new Promise(resolve => subscriber.once('message', resolve));
     await sails.helpers.redis.publish('test channel', 'test message');
 
     subscriber.on('message', this.spy);
-    await wait();
 
-    expect(this.spy).to.have.been.calledOnceWith('test channel', 'test message');
+    await p;
+
+    expect(this.spy).to.have.been.calledOnceWith('test channel', JSON.stringify('test message'));
 
 
   });
@@ -22,22 +24,16 @@ describe('Redis Pub/Sub', () => {
   it('Only receives messages on the correct channel', async () => {
     const subscriber = await sails.helpers.redis.subscribe('test channel');
 
+    const p = new Promise(resolve => subscriber.once('message', resolve));
     await sails.helpers.redis.publish('test channel', 'test message');
     await sails.helpers.redis.publish('other test', 'bad test message');
 
     subscriber.on('message', this.spy);
-    await wait();
 
-    expect(this.spy).to.have.been.calledOnceWith('test channel', 'test message');
+    await p;
+
+    expect(this.spy).to.have.been.calledOnceWith('test channel', JSON.stringify('test message'));
     expect(this.spy).to.have.calledOnce;
   });
 
 });
-
-// TODO: Get rid of this wait thing and use Promise resolving
-// Not sure how to do it atm
-async function wait() {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 250);
-  });
-}
