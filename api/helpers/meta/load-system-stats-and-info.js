d414534a43fdd067f0c632c9c133a1cc4e07ea9c
niekcandaele <22315101+niekcandaele@users.noreply.github.com>
2020-11-10 11:01:32 +0100

    Remove a lot of unused stats

--- api/helpers/meta/load-system-stats-and-info.js
+++ api/helpers/meta/load-system-stats-and-info.js
@@ -28,57 +28,15 @@ module.exports = {
     let amountOfServers = await SdtdServer.count();
     let amountOfPlayers = await Player.count();
 
-    let amountOfTeleports = await PlayerTeleport.count();
-    let amountOfTimesTeleported = await PlayerTeleport.sum('timesUsed');
-
-    let amountOfCustomCommands = await CustomCommand.count();
-    let amountOfCustomCommandsExecuted = await PlayerUsedCommand.count();
-    let amountOfCustomHooks = await CustomHook.count();
 
     let uptime = hhmmss(process.uptime());
-
-    let amountOfChatBridges = sails.hooks.discordchatbridge.getAmount();
-    let amountOfCountryBans = sails.hooks.countryban.getAmount();
-    let amountOfIngameCommandHandlers = sails.hooks.sdtdcommands.getAmount();
-
-    let gblEntries = await BanEntry.count();
-    let cronJobs = await CronJob.count({
-      enabled: true
-    });
-    let pingKickers = await SdtdConfig.count({
-      pingKickEnabled: true
-    });
-    let openTickets = await SdtdTicket.count({
-      status: true
-    });
-    let closedTickets = await SdtdTicket.count({
-      status: false
-    });
-
-    let gblComments = await GblComment.count({
-      deleted: false
-    });
 
     let response = {
       servers: amountOfServers,
       players: amountOfPlayers,
       guilds: amountOfDiscordGuilds,
       users: amountOfOnlineDiscordUsers,
-      uptime: uptime,
-      chatBridges: amountOfChatBridges,
-      countryBans: amountOfCountryBans,
-      sdtdCommands: amountOfIngameCommandHandlers,
-      customHooks: amountOfCustomHooks,
-      amountOfTimesTeleported: amountOfTimesTeleported,
-      amountOfTeleports: amountOfTeleports,
-      amountOfCustomCommands: amountOfCustomCommands,
-      amountOfCustomCommandsExecuted: amountOfCustomCommandsExecuted,
-      gblEntries: gblEntries,
-      cronJobs: cronJobs,
-      pingKickers: pingKickers,
-      openTickets: openTickets,
-      closedTickets: closedTickets,
-      gblComments: gblComments,
+      uptime: uptime
     };
 
     return exits.success(response);
