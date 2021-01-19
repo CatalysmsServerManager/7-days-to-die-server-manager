a9fe75ca116e5bebdd5d538098834459ac05cd67
Niek Candaele <niekcandaele@gmail.com>
2021-01-19 14:14:26 +0100

    Fix logging levels of Sails

--- config/log.js
+++ config/log.js
@@ -13,11 +13,19 @@
 // Load env vars
 require('dotenv').config();
 
+// Sails internal logger (captainslog) and Winstons log levels are different
+// Which results in very weird behaviour (Eg setting logLevel to 'debug' blocks 'info' and 'warning' level messages)
+// Here we manually fix sails' logger levels to reflect those of winston
+const captainsLogDefaults = require('captains-log/lib/defaults');
+captainsLogDefaults.OPTIONS.logLevels.info = 3;
+captainsLogDefaults.OPTIONS.logLevels.debug = 2;
+
 module.exports.log = {
   level: process.env.CSMM_LOGLEVEL || 'info',
 
   // Pass in our custom logger, and pass all log levels through.
   custom: require('./customLog').customLogger,
+
 
   //  Disable captain's log so it doesn't prefix or stringify our meta data.
   inspect: false
