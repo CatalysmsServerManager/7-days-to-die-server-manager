a04f16a400a64ce4d9bbceb3fe207cc3fee6a679
Niek Candaele <niekcandaele@gmail.com>
2021-02-27 11:26:03 +0100

    test: add a test for handlebars syntax

--- test/unit/helpers/sdtd/execute-custom-command.test.js
+++ test/unit/helpers/sdtd/execute-custom-command.test.js
@@ -25,7 +25,13 @@ describe('HELPER execute-custom-command', function () {
     expect(res).to.have.length(1);
     expect(res[0]).to.be.eql({ result: 'it worked yay' });
     expect(sails.helpers.sdtdApi.executeConsoleCommand.getCall(0).lastArg).to.be.equal(`say "Hello ${sails.testPlayer.steamId}"`);
+  });
 
+  it('Can handle handlebars template syntax', async function () {
+    const res = await sails.helpers.sdtd.executeCustomCmd(sails.testServer, 'say "Hello {{ player.steamId }}"', { player: sails.testPlayer });
+    expect(res).to.have.length(1);
+    expect(res[0]).to.be.eql({ result: 'it worked yay' });
+    expect(sails.helpers.sdtdApi.executeConsoleCommand.getCall(0).lastArg).to.be.equal(`say "Hello ${sails.testPlayer.steamId}"`);
   });
 
 });
