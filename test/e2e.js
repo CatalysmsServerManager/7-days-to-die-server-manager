cbbed53f24e04ef56887c79ee697718c46d8cdde
niekcandaele <niek@candaele.dev>
2021-06-03 11:06:27 +0200

    ci: add a e2e testing script

---
+++ test/e2e.js
@@ -0,0 +1,25 @@
+const {upAll} = require('docker-compose');
+const path = require('path');
+const wait = require('../worker/util/wait').wait;
+const fetch = require('node-fetch');
+const assert = require('assert/strict');
+
+async function main() {
+  await upAll({ cwd: path.join(__dirname), log: true });
+  console.log('Waiting 30 seconds for everything to initialize');
+  await wait(30);
+  const response = await fetch('http://127.0.0.1:1337');
+  assert.equal(response.status, 200);
+}
+
+
+main()
+  .then(res => {
+    console.log(res);
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
+
