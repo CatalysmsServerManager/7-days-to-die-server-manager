32b8ce49a0429d92b6f95f421e6558bd16d848b7
niekcandaele <niek@candaele.dev>
2021-06-03 11:57:56 +0200

    fix: start datastores first, csmm after

--- test/e2e.js
+++ test/e2e.js
@@ -1,4 +1,4 @@
-const {upAll, logs} = require('docker-compose');
+const {upMany, logs} = require('docker-compose');
 const path = require('path');
 const wait = require('../worker/util/wait').wait;
 const fetch = require('node-fetch');
@@ -7,9 +7,17 @@ const assert = require('assert/strict');
 const composeOpts = { cwd: path.join(__dirname), log: true };
 
 async function main() {
-  await upAll(composeOpts);
-  console.log('Waiting 60 seconds for everything to initialize');
+  // First, start the datastores and migration containers
+  await upMany(['csmm-migrations', 'db', 'cache'],composeOpts);
+
+  console.log('Waiting 60 seconds for data stores to initialize');
   await countdown(60);
+  // Once all data stores are initialized, we can start CSMM itself
+  await upMany(['csmm-web', 'csmm-worker',],composeOpts);
+
+  console.log('Waiting 15 seconds for CSMM to boot');
+  await countdown(15);
+
   await logs(['csmm-web', 'csmm-worker', 'csmm-migrations', 'db', 'cache'],composeOpts);
   const response = await fetch('http://127.0.0.1:1337');
   assert.equal(response.status, 200);
