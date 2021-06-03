5fb221d0d6f4db73d23e2aff4d977693fce9099e
niekcandaele <niek@candaele.dev>
2021-06-03 11:12:46 +0200

    ci: display logs of containers

--- test/e2e.js
+++ test/e2e.js
@@ -1,4 +1,4 @@
-const {upAll} = require('docker-compose');
+const {upAll, logs} = require('docker-compose');
 const path = require('path');
 const wait = require('../worker/util/wait').wait;
 const fetch = require('node-fetch');
@@ -9,6 +9,7 @@ async function main() {
   console.log('Waiting 30 seconds for everything to initialize');
   await wait(30);
   const response = await fetch('http://127.0.0.1:1337');
+  await logs(['csmm-web', 'csmm-worker', 'csmm-migrations', 'db', 'cache']);
   assert.equal(response.status, 200);
 }
 
