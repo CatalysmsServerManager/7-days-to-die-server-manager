05c5030e2160d73bd54b161c7f1ba84acf04e929
niekcandaele <niek@candaele.dev>
2021-06-03 11:18:10 +0200

    fix: actually log the logs :)

--- test/e2e.js
+++ test/e2e.js
@@ -4,12 +4,14 @@ const wait = require('../worker/util/wait').wait;
 const fetch = require('node-fetch');
 const assert = require('assert/strict');
 
+const composeOpts = { cwd: path.join(__dirname), log: true };
+
 async function main() {
-  await upAll({ cwd: path.join(__dirname), log: true });
+  await upAll(composeOpts);
   console.log('Waiting 30 seconds for everything to initialize');
   await wait(30);
   const response = await fetch('http://127.0.0.1:1337');
-  await logs(['csmm-web', 'csmm-worker', 'csmm-migrations', 'db', 'cache']);
+  await logs(['csmm-web', 'csmm-worker', 'csmm-migrations', 'db', 'cache'],composeOpts);
   assert.equal(response.status, 200);
 }
 
