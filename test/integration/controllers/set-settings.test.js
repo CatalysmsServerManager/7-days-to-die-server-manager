2fb15269051e97c1044909374dceca574f5ccac5
niekcandaele <niek@candaele.dev>
2021-06-05 21:07:21 +0200

    feat: add a configurable server name for commands

--- test/integration/controllers/set-settings.test.js
+++ test/integration/controllers/set-settings.test.js
@@ -10,6 +10,12 @@ describe('set-settings', function () {
       const config = await SdtdConfig.findOne({ server: sails.testServer.id });
       expect(config.replyPrefix).to.be.equal('blabla');
     });
+    it('Should set the replyServerName', async function () {
+      const response = await supertest(sails.hooks.http.app).post('/api/sdtdserver/settings').send({ replyServerName: 'replyServerName', serverId: sails.testServer.id });
+      expect(response.statusCode).to.equal(200);
+      const config = await SdtdConfig.findOne({ server: sails.testServer.id });
+      expect(config.replyServerName).to.be.equal('replyServerName');
+    });
     it('Should allow empty strings to be set', async function () {
       const response = await supertest(sails.hooks.http.app).post('/api/sdtdserver/settings').send({ replyPrefix: '', serverId: sails.testServer.id });
       expect(response.statusCode).to.equal(200);
--- worker/processors/sdtdCommands/sendReply.js
+++ worker/processors/sdtdCommands/sendReply.js
@@ -1,47 +1,56 @@
 const replyTypes = require('./replyTypes');
 
 module.exports = async function sendReplyToPlayer(server, player, type, data) {
-
-  function getReplyObj(type) {
-    return replyTypes.filter(r => r.type === type)[0];
-  }
-
-  async function getMessage(replyObj) {
-    let response = await CommandReply.find({
-      type: replyObj.type,
-      server: server.id
-    });
-    if (response.length === 0) {
-      return replyObj.default;
-    }
-    return response[0].reply;
-  }
   const replyObj = getReplyObj(type);
 
   if (!replyObj) {
-    return sendMsg(type);
+    return sendMsg(server, type, player);
   } else {
 
-    let message = await getMessage(replyObj);
+    let message = await getMessage(server, replyObj);
     if (!data) {
       data = {};
     }
     data.server = server;
     data.player = player;
     message = await sails.helpers.sdtd.fillCustomVariables(message, data);
-    return sendMsg(message);
-  }
-
-
-  function sendMsg(message) {
-
-    const { replyPrefix } = server.config;
-
-    if (replyPrefix) {
-      message = `${replyPrefix} ${message}`;
-    }
-
-    command = `pm ${player.steamId} "${message}"`;
-    return sails.helpers.sdtdApi.executeConsoleCommand(SdtdServer.getAPIConfig(server), command);
+    return sendMsg(server, message, player);
   }
 };
+
+function getReplyObj(type) {
+  return replyTypes.filter(r => r.type === type)[0];
+}
+
+
+async function getMessage(server, replyObj) {
+  let response = await CommandReply.find({
+    type: replyObj.type,
+    server: server.id
+  });
+  if (response.length === 0) {
+    return replyObj.default;
+  }
+  return response[0].reply;
+}
+
+async function sendMsg(server, message, player) {
+  const { replyPrefix, replyServerName } = server.config;
+  const cpmVersion = await sails.helpers.sdtd.checkCpmVersion(server.id);
+
+  console.log(replyServerName);
+
+  if (replyPrefix) {
+    message = `${replyPrefix} ${message}`;
+  }
+
+  if (cpmVersion) {
+    let serverName;
+    if (replyServerName) {serverName = replyServerName;} else {serverName = 'CSMM';}
+    command = `pm2 "${serverName}" ${player.steamId} "${message}"`;
+  } else {
+    command = `pm ${player.steamId} "${message}"`;
+  }
+
+  return sails.helpers.sdtdApi.executeConsoleCommand(SdtdServer.getAPIConfig(server), command);
+}
