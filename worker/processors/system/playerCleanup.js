fc995fd3b6121f77a533c1345a405c2655ee46af
niekcandaele <niek@candaele.dev>
2021-06-30 15:00:38 +0200

    fix: a typo

--- worker/processors/system/playerCleanup.js
+++ worker/processors/system/playerCleanup.js
@@ -4,7 +4,7 @@ module.exports = async function playerCleanup() {
   const now = new Date();
   for (const config of enabledServers) {
     // Fail safe to prevent we drop player from a different server
-    // Should never happen buy y'know...
+    // Should never happen but y'know...
     if (!config.server) { continue; };
 
     const deleteFrom = new Date(now.getFullYear(), now.getMonth(), now.getDate() - config.playerCleanupLastOnline);
