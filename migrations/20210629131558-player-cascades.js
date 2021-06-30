8616d7416d931d94420073b23dc9f7131ac28438
niekcandaele <niek@candaele.dev>
2021-06-30 14:41:36 +0200

    fix: take care of bad data before adding the FK

--- migrations/20210629131558-player-cascades.js
+++ migrations/20210629131558-player-cascades.js
@@ -2,6 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.sequelize.query(`
+    DELETE
+    FROM playerteleport 
+    WHERE player NOT IN (SELECT id FROM player)
+`)
+
+    await queryInterface.sequelize.query(`
+    DELETE
+    FROM trackinginfo 
+    WHERE player NOT IN (SELECT id FROM player)
+    OR server NOT IN (SELECT id FROM sdtdserver)
+`)
+
     await queryInterface.addConstraint('trackinginfo', {
       fields: ['player'],
       type: 'FOREIGN KEY',
@@ -11,6 +25,19 @@ module.exports = {
         field: 'id',
       },
       onDelete: 'cascade',
+      onUpdate: 'cascade',
+    })
+
+    await queryInterface.addConstraint('trackinginfo', {
+      fields: ['server'],
+      type: 'FOREIGN KEY',
+      name: 'FK_trackinginfo_server',
+      references: {
+        table: 'sdtdserver',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
     })
 
     await queryInterface.addConstraint('playerteleport', {
@@ -22,6 +49,7 @@ module.exports = {
         field: 'id',
       },
       onDelete: 'cascade',
+      onUpdate: 'cascade',
     })
   },
 
