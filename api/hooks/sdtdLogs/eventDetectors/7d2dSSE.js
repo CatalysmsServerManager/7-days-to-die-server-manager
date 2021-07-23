4d9988363433d7b9cdad9b53f44a804734c630e7
niekcandaele <niek@candaele.dev>
2021-07-23 17:39:44 +0200

    fix: remove listener from SSE

--- api/hooks/sdtdLogs/eventDetectors/7d2dSSE.js
+++ api/hooks/sdtdLogs/eventDetectors/7d2dSSE.js
@@ -6,28 +6,14 @@ class SdtdSSE extends LoggingObject {
   constructor(server) {
     super(server);
     this.SSERegex = /\d+-\d+-\d+T\d+:\d+:\d+ \d+\.\d+ INF (.+)/;
+    this.listener = this.SSEListener.bind(this);
   }
 
 
   async start() {
     this.eventSource = new EventSource(`http://${this.server.ip}:${this.server.webPort}/sse/log?adminuser=${this.server.authName}&admintoken=${this.server.authToken}`);
     this.eventSource.reconnectInterval = 30000;
-    this.eventSource.addEventListener('logLine', async data => {
-      try {
-        const parsed = JSON.parse(data.data);
-        const messageMatch = this.SSERegex.exec(parsed.msg);
-        if (messageMatch && messageMatch[1]) {
-          parsed.msg = messageMatch[1];
-        }
-        const log = handleLogLine(parsed);
-        if (log) {
-          await this.handleMessage(log);
-        }
-      } catch (error) {
-        sails.log.error(error.stack);
-      }
-
-    });
+    this.eventSource.addEventListener('logLine', this.listener);
     this.eventSource.onerror = e => {
       sails.log.warn(`SSE error for server ${this.server.id}`);
       sails.log.warn(e);
@@ -41,9 +27,27 @@ class SdtdSSE extends LoggingObject {
     if (!this.eventSource) {
       return;
     }
-
+    this.eventSource.removeEventListener(this.listener);
     this.eventSource.close();
   }
+
+  async SSEListener(data) {
+    try {
+      const parsed = JSON.parse(data.data);
+      const messageMatch = this.SSERegex.exec(parsed.msg);
+      if (messageMatch && messageMatch[1]) {
+        parsed.msg = messageMatch[1];
+      }
+      const log = handleLogLine(parsed);
+      if (log) {
+        await this.handleMessage(log);
+      }
+    } catch (error) {
+      sails.log.error(error.stack);
+    }
+
+  }
+
 }
 
 module.exports = SdtdSSE;
