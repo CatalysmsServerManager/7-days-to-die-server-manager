56fe5282944a718c906ae64ebf59cbc6c3f82d40
niekcandaele <niek@candaele.dev>
2021-08-10 11:46:29 +0200

    feat: store lastDeathLocation for players

--- api/hooks/sdtdLogs/enrichers/index.js
+++ api/hooks/sdtdLogs/enrichers/index.js
@@ -1,15 +1,22 @@
 const enrichers = [
   require('./player'),
   require('./playerKilled'),
+  require('./playerDied'),
 ];
 
 module.exports = async (event) => {
   try {
-    const enriched = await Promise.all(enrichers.map(enricher => enricher(event)));
-    return _.merge(event, ...enriched);
+
+    let enriched = event;
+
+    for (const enricher of enrichers) {
+      enriched = await enricher(enriched);
+    }
+
+    return enriched;
   } catch (error) {
     sails.log.warn('Error trying to enrich a log line, this should be OK to fail...');
-    sails.log.error(e);
+    sails.log.error(error);
     return event;
   }
 };
