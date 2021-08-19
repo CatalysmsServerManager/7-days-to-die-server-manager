e38b6ac0742c24d70bcf100ab7fba97a053c9a2c
Niek Candaele <niek@candaele.dev>
2021-08-19 23:05:13 +0200

    feat: refactor custom command execution, implement data for playground route

--- api/controllers/Playground/execute.js
+++ api/controllers/Playground/execute.js
@@ -1,3 +1,5 @@
+const CSMMCommand = require('../../../worker/util/CSMMCommand');
+
 module.exports = {
   inputs: {
 
@@ -6,6 +8,10 @@ module.exports = {
       required: true,
     },
 
+    data: {
+      type: 'json',
+      required: true,
+    },
 
   },
 
@@ -14,9 +20,15 @@ module.exports = {
 
 
   fn: async function (inputs, exits) {
-    return exits.success({ output: inputs.template, errors: [] });
+    const command = new CSMMCommand(inputs.server, inputs.template, inputs.data);
 
+    command.data = inputs.data;
+
+    try {
+      const result =  await command.render();
+      return exits.success({ output: result, errors: [] });
+    } catch (error) {
+      return exits.success({ output: null, errors: [error] });
+    }
   }
-
-
 };
---
+++ worker/util/CSMMCommand/index.js
@@ -0,0 +1,104 @@
+const AddCurrency = require('../../../worker/util/customFunctions/addCurrency');
+const SendDiscord = require('../../../worker/util/customFunctions/sendDiscord');
+const SetRole = require('../../../worker/util/customFunctions/setRole');
+const Wait = require('../../../worker/util/customFunctions/wait');
+
+const supportedFunctions = [
+  new Wait(),
+  new AddCurrency(),
+  new SetRole(),
+  new SendDiscord()
+];
+
+module.exports = class CSMMCommand {
+
+  constructor(server, template, data) {
+
+    this.server = server;
+    this.template = template;
+    this.data = data || {};
+    this.results = [];
+  }
+
+  async loadData() {
+    this.data.server = this.server;
+    this.data.server.onlinePlayers = await sails.helpers.sdtd.loadPlayerData(this.server.id, undefined, true);
+    this.data.server.stats = await sails.helpers.sdtdApi.getStats(SdtdServer.getAPIConfig(this.server));
+    return this.data;
+  }
+
+
+  async render() {
+    this.template = await sails.helpers.sdtd.parseCommandsString(
+      this.template,
+      this.data
+    );
+    return this.template;
+  }
+
+
+  async execute() {
+    for (const command of this.template) {
+      // Check if the command matches any custom functions
+      const customFunction = checkForCustomFunction(command);
+      if (customFunction) {
+        // If we find a custom function, execute it
+        const [result, customFunctionArgs] = await executeCustomFunction(customFunction, command, this.server);
+        this.results.push({
+          command: customFunction.name,
+          parameters: command,
+          result,
+          customFunctionArgs
+        });
+      } else {
+        // If the command is not a custom function, execute it as a game command
+        let commandResult = await executeCommand(this.server, command);
+        this.results.push(commandResult);
+      }
+    }
+    return this.results;
+  }
+
+};
+
+
+function getArgs(fnc, command) {
+  const regex = new RegExp(`${fnc.name}\((.*)\)`, 'i');
+  const res = command.match(regex);
+  return res[1].slice(1, res[1].length - 1);
+}
+
+async function executeCustomFunction(fnc, command, server) {
+  const args = getArgs(fnc, command);
+  try {
+    const res = await fnc.run(server, args);
+    return [res, args];
+  } catch (error) {
+    return [error.message, args];
+  }
+}
+
+function checkForCustomFunction(command) {
+  for (const fnc of supportedFunctions) {
+    if (command.toLowerCase().includes(fnc.name.toLowerCase() + '(')) {
+      return fnc;
+    }
+  }
+  return false;
+}
+
+async function executeCommand(server, command) {
+  try {
+    let result = await sails.helpers.sdtdApi.executeConsoleCommand(
+      SdtdServer.getAPIConfig(server),
+      _.trim(command)
+    );
+    return result;
+  } catch (error) {
+    sails.log.error(error);
+    return {
+      command,
+      result: 'An error occurred executing the API request to the 7D2D server'
+    };
+  }
+}
