d160fcd644506da4b3924b50936e12b6379e7bb8
jannishartwig <j.hartwig@tabya.de>
2021-10-06 18:36:58 +0200

    fix(Players Overview & Dashboard): Aggrid
    Rebuilded the Player Overview & Online Players with Aggrid
    
    Closes #890

--- api/controllers/SdtdServer/get-players-datatable.js
+++ api/controllers/SdtdServer/get-players-datatable.js
@@ -1,48 +1,39 @@
 // If you don't know wtf is going on here
 // https://datatables.net/manual/server-side
 
+const sevenDays = require('machinepack-7daystodiewebapi');
+
 async function getPlayersDataTable(req, res) {
+
   const whereObj = {
     server: req.body.serverId
   };
   const totalPlayers = await Player.count(whereObj);
+
+  if (!!req.body.searchText) {
+    whereObj.or = [
+      { name: { contains: req.body.searchText } },
+      { steamId: { contains: req.body.searchText } },
+      { ip: { contains: req.body.searchText } }
+    ]
+    if (!isNaN(req.body.searchText)) {
+      whereObj.or.push({ entityId: req.body.searchText });
+    }
+  }
+  if (req.body.onlyOnline === 'true') {
+    const server = await SdtdServer.findOne(req.body.serverId);
+    const playerList = await getPlayerList(server);
+    let steamIds = playerList.players.filter(p => p.online).map(p => p.steamid);
+    whereObj.steamId = { in: steamIds }
+  }
   const rowCount = parseInt(req.body.endRow) - parseInt(req.body.startRow);
   const queryObj = {
     where: whereObj,
-    //select: req.body.columns.map(c => c.data),
+    select: req.body.fields,
     sort: req.body.sortModel?.map(col => { return  getSortCondition(col) }),
     skip: req.body.startRow,
     limit: rowCount,
   };
-
-  if (!_.isEmpty(req.body.search?.value)) {
-    whereObj.or = [{
-      name: {
-        contains: req.body.search.value
-      }
-    },
-    {
-      steamId: {
-        contains: req.body.search.value
-      }
-    },
-    {
-      ip: {
-        contains: req.body.search.value
-      }
-    }
-    ];
-
-
-  }
-
-  /*if (req.body.order?.length) {
-    const colNumToSort = parseInt(req.body.order[0].column);
-    const sortObj = {};
-    sortObj[req.body.columns[colNumToSort].data] = req.body.order[0].dir;
-    queryObj.sort = [sortObj];
-  }*/
-
 
   const players = await Player.find(queryObj);
 
@@ -78,11 +69,35 @@ function getSortCondition (col) {
   let condition;
   if (col.colId.includes('.')) {
     const parts = col.colId.split('.');
-    condition = { [parts[0]]: { [parts[1]]: col.sort} };
+    condition = { [parts[0]]: col.sort };
   } else {
       condition = { [col.colId]: col.sort }
   }
   return condition;
+}
+
+/**
+ * Get Players List from 7dtd Server
+ * @param server
+ */
+async function getPlayerList(server) {
+  return new Promise((resolve) => {
+    sevenDays.getPlayerList({
+      ip: server.ip,
+      port: server.webPort,
+      authName: server.authName,
+      authToken: server.authToken
+    }).exec({
+      error: function () {
+        resolve({
+          players: []
+        });
+      },
+      success: function (playerList) {
+        resolve(playerList);
+      }
+    });
+  });
 }
 
 module.exports = getPlayersDataTable;
