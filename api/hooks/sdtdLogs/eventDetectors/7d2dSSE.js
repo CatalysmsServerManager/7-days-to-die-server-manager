a934483282dfcca614faf049132f8f7352a36e19
Niek Candaele <niek@candaele.dev>
2023-02-11 16:37:48 +0100

    fix: do not clear isConnecting flag on errors
    There's many reasons that an error occurs and we should not always clear this flag.
    This change will cause the system to be a bit slower when having to retry connections, but will be more fault tolerant

--- api/hooks/sdtdLogs/eventDetectors/7d2dSSE.js
+++ api/hooks/sdtdLogs/eventDetectors/7d2dSSE.js
@@ -111,7 +111,6 @@ class SdtdSSE extends LoggingObject {
     this.eventSource.onerror = e => {
       clearTimeout(isConnectingTimeout);
       sails.log.warn(`SSE error for server ${this.server.id}`, { server: this.server, error: e });
-      this.isConnecting = false;
     };
     this.eventSource.onopen = () => {
       clearTimeout(isConnectingTimeout);
@@ -129,6 +128,7 @@ class SdtdSSE extends LoggingObject {
     this.eventSource.removeEventListener('logLine', this.listener);
     this.eventSource.close();
     this.eventSource = null;
+    this.isConnecting = false;
   }
 
   async SSEListener(data) {
