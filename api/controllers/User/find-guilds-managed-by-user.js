ff9094312d0e447f5d68e5450d05c28757e61937
Niek Candaele <niek@candaele.dev>
2023-02-12 12:00:00 +0100

    feat: increase performance of managedGuilds API call

--- api/controllers/User/find-guilds-managed-by-user.js
+++ api/controllers/User/find-guilds-managed-by-user.js
@@ -34,35 +34,47 @@ module.exports = {
   fn: async function (inputs, exits) {
 
     try {
+      const REDIS_KEY = `user:${inputs.userId}:guilds`;
 
-      const discordClient = sails.helpers.discord.getClient();
-      const foundUser = await User.findOne(inputs.userId);
+      let cachedGuilds = await sails.helpers.redis.get(REDIS_KEY);
 
-      if (_.isUndefined(foundUser) || '' === foundUser.discordId) {
-        return exits.badRequest();
-      }
+      if (!cachedGuilds) {
+        const discordClient = sails.helpers.discord.getClient();
+        const foundUser = await User.findOne(inputs.userId);
 
-      const discordUser = await discordClient.users.fetch(foundUser.discordId);
-
-      if (discordUser === undefined || discordUser === null) {
-        return exits.badRequest();
-      }
-
-      const filteredGuilds = (await Promise.all(discordClient.guilds.cache.map(async g => {
-        try {
-          const member = await g.members.fetch(discordUser.id);
-          if (member && member.permissions.has(PermissionFlagsBits.ManageGuild)) {
-            return g;
-          }
-        } catch (error) {
-          if (error.message.includes('Unknown Member')) { return null; }
-          throw error;
+        if (_.isUndefined(foundUser) || '' === foundUser.discordId) {
+          return exits.badRequest();
         }
 
-      }))).filter(Boolean);
+        const discordUser = await discordClient.users.fetch(foundUser.discordId);
 
-      sails.log.debug(`API - SdtdServer:find-guilds-managed-by-user - Found ${filteredGuilds.length} guilds for user ${inputs.userId}!`, { userId: inputs.userId });
-      return exits.success(filteredGuilds);
+        if (discordUser === undefined || discordUser === null) {
+          return exits.badRequest();
+        }
+
+        cachedGuilds = (await Promise.all(discordClient.guilds.cache.map(async g => {
+          try {
+            const member = await g.members.fetch(discordUser.id);
+            if (member && member.permissions.has(PermissionFlagsBits.ManageGuild)) {
+              return { id: g.id, name: g.name };
+            }
+          } catch (error) {
+            if (error.message.includes('Unknown Member')) { return null; }
+            throw error;
+          }
+
+        }))).filter(Boolean);
+
+        if (cachedGuilds.length) {
+          await sails.helpers.redis.set(REDIS_KEY, JSON.stringify(cachedGuilds), true, 60 * 60 * 24);
+        }
+
+      } else {
+        cachedGuilds = JSON.parse(cachedGuilds);
+      }
+
+      sails.log.debug(`API - SdtdServer:find-guilds-managed-by-user - Found ${cachedGuilds.length} guilds for user ${inputs.userId}!`, { userId: inputs.userId });
+      return exits.success(cachedGuilds);
     } catch (error) {
       sails.log.error(`API - SdtdServer:find-guilds-managed-by-user - ${error}`, { userId: inputs.userId });
       return exits.error(error);
